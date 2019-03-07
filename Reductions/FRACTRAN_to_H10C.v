3bc2e34c4efc391758aff2decd504660cb0571f5
Dominique Larchey-Wendling <larchey@gmail.com>
2019-03-07 14:45:11 +0100

    some comments about the encoding DIO_ELEM -> H10C

--- Reductions/FRACTRAN_to_H10C.v
+++ Reductions/FRACTRAN_to_H10C.v
@@ -16,20 +16,50 @@ Require Import FRACTRAN_DIO H10C.
 
 Set Implicit Arguments.
 
-Section DIO_ELEM_H10C.
+Section dc_list_h10c.
+
+  (** Reduction from (l,ν) an instance of a DIO_ELEM_PROBLEM 
+      where 
+         - l is a list of elementary Diophantine constraints
+         - ν is a valuation for the paratemeters
+      into an instance of H10C_PROBLEM, i.e.
+         - a list of x=1 or x=y+z or x=y*z
+    *)
 
   Variable (ν : nat -> nat).
+
+  (* Giving a valuation for variables φ : nat -> nat, we define
+     Ψ : nat -> nat such that
+     - Ψ 0 = 1 (index zero represents constant 1)
+     - Ψ (2n+2) = n (index 2n+2 represents constant n)
+     - Ψ (2n+1) = φ n (index 2n+1 represents the value of variable n)
+
+     The encoding goes like this:
+
+       - we add   x _0 = 1, 
+                  x_2 + x_2 = x_2
+                  x_2 + x_0 = x_4
+                  ...
+                  x_2n+2 + x_0 = x_2n
+         until a large enough n to covert all used constants
+         and parameter values
+       - we encode
+             x_i = c       ~~> x_2i+1 = x_2c+2
+             x_i = x_j     ~~> x_2i+1 + x_2 = x_2j+1
+             x_i = p       ~~> x_2i+1 = x_{2*(ν p)+2}
+             x_i = x_j%x_k ~~> x_2j+1 + x_2k=1 = x_2i+1  
+   *) 
 
   Let even n := 2*n+2.
   Let odd n := 2*n+1.
 
-  Definition h10c_nat n :=
+  Let h10c_nat n :=
     match n with
       | 0   => h10c_plus (even 0) (even 0) (even 0)
       | S n => h10c_plus (even n) 0 (even (S n))
     end.
 
-  Definition dc_h10c (c : dio_constraint) :=
+  Let dc_h10c (c : dio_constraint) :=
     let (x,c) := c 
     in match c with
       | dee_nat n   => h10c_plus (even n) (even 0) (odd x)
@@ -39,33 +69,34 @@ Section DIO_ELEM_H10C.
       | dee_comp do_mul y z => h10c_mult (odd y) (odd z) (odd x) 
     end.
 
-  Definition dee_const c :=
+  Let dee_const c :=
     match c with
       | dee_nat n   => n::nil
       | dee_par p   => ν p::nil
       | _           => nil
     end.
 
-  Section local.
+  Section dc_h10c_equiv.
 
-  Variable (φ Ψ : nat -> nat)
-           (Hpsy_0 : Ψ 0 = 1) 
-           (Hpsy_odd : forall n, Ψ (odd n) = φ n).
+    Variable (φ Ψ : nat -> nat)
+             (Hpsy_0 : Ψ 0 = 1) 
+             (Hpsy_odd : forall n, Ψ (odd n) = φ n).
           
-  Theorem dc_h10c_eq c : (forall n, In n (0::dee_const (snd c)) -> Ψ (even n) = n)
-                     -> dc_eval φ ν c <-> h10c_sem (dc_h10c c) Ψ.
-  Proof.
-    destruct c as (x,[ n | y | p | [] y z ]); unfold dc_eval; simpl; intros H.
-    + do 2 (rewrite H; auto); rewrite Hpsy_odd; omega.
-    + rewrite H; auto; do 2 rewrite Hpsy_odd; omega.
-    + do 2 (rewrite H; auto); rewrite Hpsy_odd; omega.
-    + do 3 rewrite Hpsy_odd; omega.
-    + do 3 rewrite Hpsy_odd; omega.
-  Qed.
+    Local Lemma dc_h10c_equiv c : 
+                (forall n, In n (0::dee_const (snd c)) -> Ψ (even n) = n)
+             -> dc_eval φ ν c <-> h10c_sem (dc_h10c c) Ψ.
+    Proof.
+      destruct c as (x,[ n | y | p | [] y z ]); unfold dc_eval; simpl; intros H.
+      + do 2 (rewrite H; auto); rewrite Hpsy_odd; omega.
+      + rewrite H; auto; do 2 rewrite Hpsy_odd; omega.
+      + do 2 (rewrite H; auto); rewrite Hpsy_odd; omega.
+      + do 3 rewrite Hpsy_odd; omega.
+      + do 3 rewrite Hpsy_odd; omega.
+    Qed.
 
-  End local.
+  End dc_h10c_equiv.
 
-  Fixpoint dc_max (l : list dio_constraint) :=
+  Let Fixpoint dc_max (l : list dio_constraint) :=
     match l with 
       | nil => 0
       | (_,dee_nat n)::l => max n (dc_max l)
@@ -73,9 +104,9 @@ Section DIO_ELEM_H10C.
       | _::l             => dc_max l
     end.
 
-  Definition dc_list_const l := list_an 0 (1+dc_max l).
+  Let dc_list_const l := list_an 0 (1+dc_max l).
 
-  Fact dc_list_const_prop c l : In c l -> incl (0::dee_const (snd c)) (dc_list_const l).
+  Let dc_list_const_prop c l : In c l -> incl (0::dee_const (snd c)) (dc_list_const l).
   Proof.
     intros Hc x [ Hx | Hx ]; apply list_an_spec.
     + subst; omega.
@@ -129,7 +160,7 @@ Section DIO_ELEM_H10C.
         rewrite Hpsy_0; omega.
       * intros c; rewrite in_map_iff.
         intros (k & H1 & H2).
-        rewrite <- H1, <- dc_h10c_eq with (φ := phi); auto.
+        rewrite <- H1, <- dc_h10c_equiv with (φ := phi); auto.
         rewrite Forall_forall in Hphi; auto.
     + intros (psy & Hpsy).
       simpl in Hpsy.
@@ -142,7 +173,7 @@ Section DIO_ELEM_H10C.
       exists (fun n => psy (odd n)).
       rewrite Forall_forall.
       intros c Hc.
-      rewrite dc_h10c_eq with (Ψ := psy); auto.
+      rewrite dc_h10c_equiv with (Ψ := psy); auto.
       * apply H3, in_map_iff; exists c; auto.
       * assert (forall n, n <= dc_max l -> h10c_sem (h10c_nat n) psy) as H4.
         { intros n Hn; apply H2, in_map_iff; exists n; split; auto.
@@ -160,27 +191,24 @@ Section DIO_ELEM_H10C.
         induction n as [ | n IHn ]; intros Hn; specialize (H2 _ Hn); simpl in H2. 
         - omega.
         - rewrite IHn, H1 in H2; omega.
-  Qed. 
+  Qed.
 
-End DIO_ELEM_H10C.
+End dc_list_h10c. 
 
-Section Reduction.
+Section DIO_ELEM_H10C_SAT.
 
-  Let f : DIO_ELEM_PROBLEM -> H10C_PROBLEM.
-  Proof.
-    intros (l,ν). 
-    exact (dc_list_h10c ν l).
-  Defined.
+  Let f (P : DIO_ELEM_PROBLEM) : H10C_PROBLEM :=
+     let (l,ν) := P in dc_list_h10c ν l.
 
   Theorem DIO_ELEM_H10C_SAT : DIO_ELEM_SAT ⪯ H10C_SAT.
   Proof.
     exists f.
-    intros (l,ν); simpl.
-    unfold H10C_SAT; rewrite dc_list_h10c_spec.
-    split; intros (phi & Hphi); exists phi; revert Hphi; rewrite Forall_forall; auto.
+    intros (l,ν); simpl; unfold H10C_SAT; rewrite dc_list_h10c_spec.
+    split; intros (phi & Hphi); exists phi; revert Hphi; 
+      rewrite Forall_forall; auto.
   Qed.
 
-End Reduction.
+End DIO_ELEM_H10C_SAT.
 
 Print h10c.
 Print h10c_sem.
