14b175161c431459f868f8ec60d4afa71fb3902c
Dominik Kirst <kirst@ps.uni-saarland.de>
2019-10-15 18:57:18 +0200

    kripke admissibility

--- theories/KripkeCompleteness.v
+++ theories/KripkeCompleteness.v
@@ -1,11 +1,11 @@
-(** ** Completeness **)
+(** ** Constructive Analysis of Completeness Theorems **)
 
 From Undecidability.FOLC Require Export Gentzen.
 From Undecidability.FOLC Require Export Kripke.
 From Undecidability.FOLC Require Export GenCompleteness.
 From Undecidability.FOLC Require Export Stability.
 
-(* *** Exploding and Minimal Models *)
+(** *** Exploding and Minimal Models *)
 
 Section KripkeCompleteness.
   Context {Sigma : Signature}.
@@ -104,13 +104,11 @@ Section KripkeCompleteness.
     Qed.
   End BottomlessCompleteness.
 
-(* *** Standard Models *)
+(** *** Standard Models *)
 
   Section StandardCompleteness.
     Context {HdF : eq_dec Funcs} {HdP : eq_dec Preds}.
     Context {HeF : enumT Funcs} {HeP : enumT Preds}.
-
-    Variable sprv_stab : forall A psi phi, stable (sprv expl A psi phi).
 
     Definition cons A := ~ A ⊢SE ⊥.
     Definition cons_ctx := { A | cons A }.
@@ -136,49 +134,68 @@ Section KripkeCompleteness.
       {|
         reachable := ctx_incl ;
         k_interp := universal_interp ;
-        k_P := fun A P v => A ⊢SC (Pred P v) ;
-        k_Bot := fun A => A ⊢SC ⊥
+        k_P := fun A P v => ~ ~ A ⊢SC (Pred P v) ;
+        k_Bot := fun _ => False
       |}.
     Proof.
       - abstract eauto.
       - abstract (cctx; firstorder).
-      - abstract (eauto using seq_Weak).
+      - intros A B H P t H1 H2. apply H1. intros H3. apply H2.
+        abstract (eauto using seq_Weak).
       - abstract (cctx; firstorder).
     Defined.
 
     Lemma nsprv_cons A phi :
-      (forall H, (exist cons A H) ⊢SC phi) -> A ⊢SE phi.
+      (forall H, (exist cons A H) ⊢SC phi) -> ~ ~ A ⊢SE phi.
     Proof.
-      intros Hcon. apply sprv_stab. intros H. apply H. eauto.
+      intros Hsat H.
+      assert (HA : ~ ~ (A ⊢SE ⊥ \/ ~ A ⊢SE ⊥)) by tauto.
+      apply HA. clear HA. intros [HA|HA].
+      - apply H. apply Absurd. assumption.
+      - apply H, (Hsat HA).
     Qed.
 
     Lemma K_std_correct (A : cons_ctx) rho phi :
-      (rho ⊨(A, K_std) phi -> A ⊢SC phi[rho]) /\
+      (rho ⊨(A, K_std) phi -> ~ ~ A ⊢SC phi[rho]) /\
       ((forall B psi, A <<=C B -> B ;; phi[rho] ⊢sC psi -> B ⊢SC psi) -> rho ⊨(A, K_std) phi).
     Proof.
-      revert A rho; enough ((forall A rho, rho ⊨( A, K_std) phi -> A ⊢SC phi[rho]) /\ (forall A rho, (forall B psi, A <<=C B -> B;; phi[rho] ⊢sC psi -> B ⊢SC psi) -> rho ⊨( A, K_std) phi)) by intuition.
+      revert A rho; enough ((forall A rho, rho ⊨( A, K_std) phi -> ~ ~ A ⊢SC phi[rho])
+                          /\ (forall A rho, (forall B psi, A <<=C B -> B;; phi[rho] ⊢sC psi -> ~ ~ B ⊢SC psi)
+                                    -> rho ⊨( A, K_std) phi)) by firstorder.
       induction phi as [| t1 t2 | phi [IHphi1 IHphi2] psi [IHpsi1 IHpsi2] | phi [IHphi1 IHphi2]].
       all: cbn; asimpl; split; intros A rho.
       - tauto.
-      - intros H. apply H; eauto.
+      - intros H. exfalso. apply (H A ⊥); auto.
       - now rewrite (vec_ext (fun x => universal_interp_eval rho x)).
-      - rewrite (vec_ext (fun x => universal_interp_eval rho x)). intros H. apply H; eauto.
-      - intros Hsat. apply IR. apply nsprv_cons. intros Hcon. apply IHpsi1, Hsat, IHphi2; cctx.
-        1: intuition. eauto.
-      - intros H B HB Hphi % IHphi1. apply IHpsi2. intros C xi HC Hxi. apply H; cctx.
-        1: now transitivity B. eauto using seq_Weak.
+      - rewrite (vec_ext (fun x => universal_interp_eval rho x)). intros H H'.
+        eapply H. 3: { intros H1. apply H', H1. } all: auto.
+      - intros Hsat H.
+        assert (HA : ~ ~ ((phi[rho] :: proj1_sig A) ⊢SE ⊥ \/ ~ (phi[rho] :: proj1_sig A) ⊢SE ⊥)) by tauto.
+        apply HA. clear HA. intros [HA|HA].
+        + apply H. apply IR. apply Absurd. assumption.
+        + pose (A' := exist cons (phi[rho] :: proj1_sig A) HA). apply (IHpsi1 A' rho).
+          * apply Hsat; auto 3. apply IHphi2. intros B theta HB HT.
+            intros H'. apply H'. eauto.
+          * intros H'. apply H. apply IR, H'.
+      - intros H B HB Hphi % IHphi1. apply IHpsi2. intros C xi HC Hxi.
+        intros HX. apply Hphi. intros Hphi'. apply (H C xi); trivial.
+        + cctx. now transitivity B.
+        + apply IL; trivial. eapply seq_Weak; eauto.
       - pose (phi' := subst_form (var_term 0 .: (rho >> subst_term (S >> var_term))) phi).
-        intros Hsat. apply AllR. cctx. destruct (find_unused_L (phi' :: A)).
+        intros Hsat. intros H. cctx. destruct (find_unused_L (phi' :: A)).
+        apply (IHphi1 (exist cons A HA) (var_term x.:rho)).
+        rewrite ksat_ext. 2: reflexivity. now apply Hsat.
+        intros H'. apply H, AllR.
         eapply seq_nameless_equiv with (n := x) (phi0 := phi').
         + intros xi Hxi. apply u. constructor. intuition.
         + apply u. omega. intuition.
-        + asimpl. apply (IHphi1 (exist cons A HA)). 1: rewrite ksat_ext. 2: reflexivity. now apply Hsat.
+        + asimpl. apply H'.
       - intros H t. apply IHphi2. intros B psi HB Hpsi. apply H. assumption.
         apply AllL with (t0 := t). now asimpl in *.
     Qed.
 
     Corollary K_std_sprv A rho phi :
-      rho ⊨(A, K_std) phi -> A ⊢SC phi[rho].
+      rho ⊨(A, K_std) phi -> ~ ~ A ⊢SC phi[rho].
     Proof.
       now destruct (K_std_correct A rho phi).
     Qed.
@@ -192,15 +209,22 @@ Section KripkeCompleteness.
     Lemma K_std_standard :
       kstandard K_std.
     Proof.
-      intros []. apply c.
+      intros []. cbn. trivial.
     Qed.
 
     Lemma K_std_completeness A phi :
-      A ⊫KS phi -> A ⊢SE phi.
+      A ⊫KS phi -> ~ ~ A ⊢SE phi.
     Proof.
-      intros Hsat. erewrite <-idSubst_form. apply nsprv_cons. intros H. apply K_std_sprv with (rho := ids).
-      2: reflexivity. apply Hsat. 1: apply K_std_standard. intros psi Hpsi. apply K_std_ksat.
-      intros B xi HB Hxi. asimpl in Hxi. eauto.
+      intros Hsat H.
+      assert (HA : ~ ~ (A ⊢SE ⊥ \/ ~ A ⊢SE ⊥)) by tauto.
+      apply HA. clear HA. intros [HA|HA].
+      - apply H. apply Absurd. assumption.
+      - specialize (Hsat _ K_std K_std_standard (exist cons A HA) ids).
+        apply K_std_sprv in Hsat.
+        + apply Hsat. intros Hsat'. apply H.
+          rewrite <- idSubst_form with ids phi; trivial.
+        + intros psi Hpsi. apply K_std_ksat.
+          intros B xi HB Hxi. asimpl in Hxi. eauto.
     Qed.
 
     Lemma K_std_seq_ksoundness A phi :
@@ -209,6 +233,8 @@ Section KripkeCompleteness.
       intros H % seq_ND. apply @ksoundness with (b := expl). 2: apply H. firstorder.
     Qed.
   End StandardCompleteness.
+
+  (** *** MP is required *)
 
   Section MPRequired.
     Variable C : stab_class.
@@ -225,4 +251,5 @@ Section KripkeCompleteness.
       intros psi Hpsi % HT'. apply (ksat_mon Hv Hpsi).
     Qed.
   End MPRequired.
+  
 End KripkeCompleteness.
