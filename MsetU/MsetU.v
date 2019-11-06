a73f0b5d0eb35e6db4c8067f7d677a89a4b9b8ca
Andrej Dudenhefner <mrhaandi@gmail.com>
2019-11-06 10:52:48 +0100

    refactoring

--- MsetU/MsetU.v
+++ MsetU/MsetU.v
@@ -1,107 +1,56 @@
 (* 
-  Finite multiset constratins with one constant 0 and unary function h
-  (h^n 0) is represented as a natural number
+  Autor(s):
+    Andrej Dudenhefner (1) 
+  Affiliation(s):
+    (1) Saarland Informatics Campus, Saarland University, Saarbrücken, Germany
+*)
 
-  Terms t, u are
+(* 
+  Problem:
+    Finite multiset constraint unification (FMsetU)
+
+  Finite multiset constratins with one constant 0 and one unary constructor h
+  A finite multiset A is represented by a list of elements.
+  The element (h^n 0) is represented by the natural number n.
+
+  Terms t, u:
     t, u ::= n : list nat | x : nat | t ⊍ u | t ⩀ u | h (t)
     where x ranges over multiset variables
   
-  Constraints 𝒞 are
-    𝒞 ::= t = u 
+  Constraints:
+    t ≐ u 
 
-  Constraints (t1 = u1),...,(tn = un) are satisfied, if 
-    ϕ(t1) ≡ ϕ(u1),..., ϕ(tn) ≡ ϕ(un) for some ϕ : nat -> list nat
-    where ≡ is equality up to permutation 
-  *)
-
+  FMsetU:
+    Given a list (t₁ ≐ u₁),...,(tₙ ≐ uₙ) of constraints,
+    is there a valuation φ : nat -> list nat such that
+    φ(t₁) ≡ φ(u₁),..., φ(tₙ) ≡ φ(uₙ),
+    where ≡ is equality up to permutation?
+  
+  References:
+    [1] Paliath Narendran: Solving Linear Equations over Polynomial Semirings.
+      LICS 1996: 466-472, doi: 10.1109/LICS.1996.561463
+*)
+Require Import ssreflect ssrbool ssrfun.
 Require Import Arith.
+Require Import Psatz.
 Require Import List.
 Import ListNotations.
 
 
-Set Implicit Arguments.
-
-Inductive mset : Set :=
-  | mset_const : list nat -> mset
-  | mset_var : nat -> mset
-  | mset_plus : mset -> mset -> mset
-  | mset_cap : mset -> mset -> mset
-  | mset_h : mset -> mset.
-
-(*
-(* list equality up to permutation *)
-Fixpoint mset_eq (A B : list nat) : Prop := 
-  match A with
-  | [] => B = []
-  | (n :: A) => exists (B1 B2 : list nat), B = B1 ++ (n :: B2) /\ (mset_eq A (B1 ++ B2))
-  end.
-*)
-
-(* list equality up to permutation *)
-Definition mset_eq (A B : list nat) : Prop := 
-  forall c, count_occ Nat.eq_dec A c = count_occ Nat.eq_dec B c.
-Notation "A ≡ B" := (mset_eq A B) (at level 65).
-
-(* removes the first occurrence of n in A, fails otherwise *)
-Fixpoint mset_try_remove (n : nat) (A : list nat) : option (list nat) :=
-  match A with
-  | [] => None
-  | (m :: A) => if Nat.eqb n m then Some A else option_map (cons m) (mset_try_remove n A)
-  end.
-
-(* intersection of two multisets *)
-Fixpoint mset_intersect (A B : list nat) : list nat :=
-  match A with
-  | [] => []
-  | (n :: A) => 
-    match mset_try_remove n B with
-    | None => mset_intersect A B
-    | Some(B) => n :: (mset_intersect A B)
-    end
-  end.
-
-(* interpret an mset wrt. a valuation ϕ *)
-Fixpoint mset_sem (ϕ : nat -> list nat) (A : mset) : list nat :=
-  match A with
-    | mset_const l => l
-    | mset_var x => ϕ x
-    | mset_plus A B => (mset_sem ϕ A) ++ (mset_sem ϕ B)
-    | mset_cap A B => mset_intersect (mset_sem ϕ A) (mset_sem ϕ B)
-    | mset_h A => map S (mset_sem ϕ A)
-  end.
-
-(* list of constraints *)
-Definition MsetU_PROBLEM := list (mset * mset).
-(* is there a valuation ϕ that satisfies all contraints *)
-Definition MsetU_SAT (l : MsetU_PROBLEM) := 
-  exists (ϕ : nat -> list nat), forall (A B : mset), In (A, B) l -> (mset_sem ϕ A) ≡ (mset_sem ϕ B).
-
-Notation "t ⊍ u" := (mset_plus t u) (at level 40).
-Notation "t ⩀ u" := (mset_cap t u) (at level 39).
-Notation "'h' t" := (mset_h t) (at level 38).
-(*Notation "• x" := (mset_var x) (at level 38, format "• x") : nat_scope.*)
-Notation "• l" := (mset_const l) (at level 38, format "• l").
-
-Coercion mset_var : nat >-> mset.
-
-(*
-Definition MsetU_SAT_ϕ (ϕ : nat -> list nat) (l : MsetU_PROBLEM) := 
-  forall (A B : mset), In (A, B) l -> (mset_sem ϕ A) ≡ (mset_sem ϕ B).
-*)
-
-
-Definition mset_sat (ϕ : nat -> list nat) (l : MsetU_PROBLEM) := 
-  Forall (fun '(A, B) => (mset_sem ϕ A) ≡ (mset_sem ϕ B)) l.
-
-
-
-Require Import ssreflect ssrbool ssrfun.
-Require Import PeanoNat.
-Require Import Psatz.
-
 Require Import Facts.
 Require Import ListFacts.
 Require Import UserTactics.
+
+
+From Undecidability.Problems Require Import FMsetU.
+
+Notation "t ⊍ u" := (mset_term_plus t u) (at level 40).
+Notation "t ⩀ u" := (mset_term_cap t u) (at level 39).
+Notation "'h' t" := (mset_term_h t) (at level 38).
+Notation "• l" := (mset_term_const l) (at level 38, format "• l").
+
+Coercion mset_term_var : nat >-> mset_term.
+
 
 Set Implicit Arguments.
 Set Maximal Implicit Insertion.
@@ -109,17 +58,15 @@ Set Maximal Implicit Insertion.
 Arguments mset_eq !A !B.
 Arguments mset_intersect !A !B.
 
-Lemma mset_satP {φ l} : mset_sat φ l <-> (forall (A B : mset), In (A, B) l -> (mset_sem φ A) ≡ (mset_sem φ B)).
+Definition mset_sat (φ : nat -> list nat) (l : FMsetU_PROBLEM) := 
+  Forall (fun '(A, B) => (mset_sem φ A) ≡ (mset_sem φ B)) l.
+
+Lemma mset_satP {φ l} : mset_sat φ l <-> (forall (A B : mset_term), In (A, B) l -> (mset_sem φ A) ≡ (mset_sem φ B)).
 Proof.
   rewrite /mset_sat Forall_forall. constructor.
   - move=> H A B. apply /H.
   - move=> H [A B]. apply /H.
 Qed.
-
-(*
-Notation "n .+1" := (Datatypes.S n) (at level 2, left associativity, format "n .+1").
-Notation "n .-1" := (Nat.pred n) (at level 2, left associativity, format "n .-1").
-*)
 
 (* list facts *)
 
@@ -130,24 +77,64 @@ Proof.
   - move => a A /=. case. move /length_zero_iff_nil => ->. by eexists.
 Qed.
 
+Lemma nil_or_ex_max (A : list nat) : A = [] \/ exists a, In a A /\ Forall (fun b => a >= b) A.
+Proof.
+  elim: A.
+    by left.
+  move=> a A. case.
+    move=> ->. right. exists a. split; by [left | constructor].
+  move=> [b [Hb1 Hb2]]. right.
+  case: (le_lt_dec a b)=> ?.
+  - exists b. split; by [right | constructor].
+  - exists a. split.
+      by left.
+    constructor.
+      done.
+    move: Hb2. apply /Forall_impl => ?. by lia.
+Qed.
+
+(* count_occ facts *)
+Lemma count_occ_app {X : Type} {D : forall x y : X, {x = y} + {x <> y}} {A B c}:
+count_occ D (A ++ B) c = count_occ D A c + count_occ D B c.
+Proof.
+  elim: A B.
+    done.
+  move=> a A IH B /=. rewrite IH. by case: (D a c).
+Qed.
+
+Lemma count_occ_cons {X : Type} {D : forall x y : X, {x = y} + {x <> y}} {A a c}:
+count_occ D (a :: A) c = count_occ D (locked [a]) c + count_occ D A c.
+Proof.
+  rewrite /count_occ /is_left -lock. by case: (D a c).
+Qed.
+
+(* Forall facts *)
+Lemma Forall_nil_iff {X: Type} {P: X -> Prop} : Forall P [] <-> True.
+Proof. by constructor. Qed.
+
+(* seq facts *)
+Lemma seq_last start length : seq start (S length) = (seq start length) ++ [start + length].
+Proof.
+  elim : length start.
+    move => * /=. f_equal. by lia.
+  move => length IH start /=. f_equal.
+  have -> : start + S length = (S start) + length by lia.
+  by apply : IH.
+Qed.
+
+
+(* repeat facts *)
+Lemma repeat_add {X : Type} {x : X} {m n} : repeat x (m + n) = repeat x m ++ repeat x n.
+Proof.
+  elim: m.
+    done.
+  move=> m IH. cbn. by f_equal.
+Qed.
 
 (*
 Section Mset.
 *)
 
-(*
-Proof.
-  elim : A B.
-  - by move=> B ->.
-  - move=> n A IH B. rewrite /mset_eq -/mset_eq. move=> [B1 [B2 [-> +]]].
-    move /IH => /= ->. rewrite ? app_length => /=. by lia.
-Qed.  
-  *)
-(*
-  Lemma eq_in {A B} : A ≡ B -> forall c, (In c A <-> In c B).
-  Proof.
-  Admitted.
-  *)
   Lemma eq_refl {A} : A ≡ A.
   Proof.
     done.
@@ -163,32 +150,6 @@ Qed.
     by firstorder done.
   Qed.
   
-  
-
-  (*
-  Lemma eq_app {A B} : A ++ B ≡ B ++ A.
-  Proof.
-  Admitted.
-
-  Lemma eq_cons {a A} : a :: A ≡ A ++ [a].
-  Proof.
-    rewrite -/(app [a] A). by apply: eq_app.
-  Qed.
-  *)
-
-  Lemma eq_consE {a A B} : (a :: A) ≡ B -> exists B1 B2, B = B1 ++ (a :: B2) /\ A ≡ (B1 ++ B2).
-  Proof.
-  Admitted.
-
-
-
-  Lemma eq_map_iff {f} {A B}: map f A ≡ map f B <-> A ≡ B.
-  Proof.
-  Admitted.
-
-
-
-
   Lemma eq_in_iff {A B} : A ≡ B -> forall c, In c A <-> In c B.
   Proof.
     rewrite /mset_eq => H c. constructor=> Hc.
@@ -209,7 +170,7 @@ Qed.
     done.
   Qed.
 
-  Lemma mset_eq_trans {A B C} : A ≡ B -> B ≡ C -> A ≡ C.
+  Lemma eq_trans {A B C} : A ≡ B -> B ≡ C -> A ≡ C.
   Proof.
     rewrite /mset_eq => + + c.
     move=> /(_ c) + /(_ c).
@@ -226,56 +187,12 @@ Qed.
     A ≡ A' -> B ≡ B' -> (A ≡ B) <-> (A' ≡ B').
   Proof.
     move=> HA HB. constructor.
-    - move /mset_eq_trans. move /(_ _ HB). 
-      move /(mset_eq_trans _). by move /(_ _ (eq_symm HA)). 
-    - move /mset_eq_trans. move /(_ _ (eq_symm HB)). 
-      move /(mset_eq_trans _). by move /(_ _ HA).
+    - move /eq_trans. move /(_ _ HB). 
+      move /(eq_trans _). by move /(_ _ (eq_symm HA)). 
+    - move /eq_trans. move /(_ _ (eq_symm HB)). 
+      move /(eq_trans _). by move /(_ _ HA).
   Qed. 
 
-
-  Lemma count_occ_app {X : Type} {D : forall x y : X, {x = y} + {x <> y}} {A B c}:
-    count_occ D (A ++ B) c = count_occ D A c + count_occ D B c.
-  Proof.
-    elim: A B.
-      done.
-    move=> a A IH B /=. rewrite IH. by case: (D a c).
-  Qed.
-  
-  (*
-  Lemma count_occ_cons {X : Type} {D : forall x y : X, {x = y} + {x <> y}} {A a c}:
-    count_occ D (a :: A) c = (if D a c then 1 else 0) + count_occ D A c.
-  Proof.
-    rewrite /count_occ /is_left. by case: (D a c).
-  Qed.
-  *)
-  
-  Lemma count_occ_cons {X : Type} {D : forall x y : X, {x = y} + {x <> y}} {A a c}:
-    count_occ D (a :: A) c = count_occ D (locked [a]) c + count_occ D A c.
-  Proof.
-    rewrite /count_occ /is_left -lock. by case: (D a c).
-  Qed.
-  
-  
-  
-  Lemma app_cons_spec {X : Type} {a : X} {A : list X} : a :: A = (locked [a]) ++ A.
-  Proof. by rewrite -lock. Qed.
-  
-  
-
-
-  Lemma Forall_nil_iff {X: Type} {P: X -> Prop} : Forall P [] <-> True.
-  Proof. by constructor. Qed.
-
-
-  Lemma or_arg {P Q R: Prop} : (P \/ Q -> R) <-> (P -> R) /\ (Q -> R).
-  Proof.
-    by firstorder done.
-  Qed.
-
-  Lemma or_idemp {P: Prop} : (P \/ P) <-> P.
-  Proof.
-    by tauto.
-  Qed.
 
   Lemma eq_appI {A B A' B'} : A ≡ A' -> B ≡ B' -> A ++ B ≡ A' ++ B'.
   Proof.
@@ -283,6 +200,7 @@ Qed.
     rewrite ? count_occ_app. by move=> -> ->.
   Qed.
 
+  
   (* solves trivial multiset equalities *)
   Ltac mset_eq_trivial := 
     move=> ?; rewrite ? (count_occ_app, count_occ_cons, count_occ_nil); unlock; by lia.
@@ -344,42 +262,6 @@ Qed.
 *)
 
 
-
-
-
-Lemma seq_last start length : seq start (S length) = (seq start length) ++ [start + length].
-Proof.
-  elim : length start.
-    move => * /=. f_equal. by lia.
-  move => length IH start /=. f_equal.
-  have -> : start + S length = (S start) + length by lia.
-  by apply : IH.
-Qed.
-
-Lemma cons_length {X : Type} {a : X} {A : list X} : length (a :: A) = 1 + length A.
-Proof. done. Qed.
-
-
-Lemma nil_or_ex_max (A : list nat) : A = [] \/ exists a, In a A /\ Forall (fun b => a >= b) A.
-Proof.
-  elim: A.
-    by left.
-  move=> a A. case.
-    move=> ->. right. exists a. split.
-      by left.
-    by constructor.
-  move=> [b [Hb1 Hb2]]. right.
-  case: (le_lt_dec a b)=> ?.
-  - exists b. split.
-      by right.
-    by constructor.
-  - exists a. split.
-      by left.
-    constructor.
-      done.
-    move: Hb2. apply /Forall_impl => ?. by lia.
-Qed.
-
 Lemma eq_mapE {A} : map S A ≡ A -> A = [].
 Proof.
   case (nil_or_ex_max A).
@@ -435,46 +317,6 @@ Proof.
   by lia.
 Qed.
 
-(*
-Lemma mset_intersect_eq {A B C} : B ≡ C -> mset_intersect A B = mset_intersect A C.
-Proof.
-  elim: A B C.
-    done.
-  move=> a A IH B C /copy [/eq_in_iff /(_ a) HInBC] HBC.
-  rewrite /mset_intersect -/mset_intersect.
-Admitted.
-*)
-
-Lemma in_seq {a n} : a < n -> In a (seq 0 n).
-Proof.
-  elim: n.
-    by lia.
-  move=> n IH ?.
-  have : a < n \/ a = n by lia. rewrite seq_last in_app_iff /plus. 
-  case.
-    move=> /IH ?. by left.
-  move=> ->. right. by left.
-Qed.
-
-(*
-Lemma intersectP {A B C} : mset_intersect A B = C -> 
-  forall c, count_occ Nat.eq_dec C c = min (count_occ Nat.eq_dec A c) (count_occ Nat.eq_dec B c).
-Proof.
-  elim: A B C.
-    move=> B C. by move=> <-.
-  move=> a A IH B C. rewrite /mset_intersect -/mset_intersect.
-  elim: B C.
-    move=> C /= /IH + c. move=> /(_ c) -> /=. by rewrite ? Nat.min_0_r.
-  move=> b B IH2 C. rewrite /mset_try_remove -/mset_try_remove.
-  case: (Nat.eq_dec a b) => ?; inspect_eqb.
-    subst b. case: C.
-      done.
-    move=> c C. case=> ?. subst c.
-    move=> /IH + c. move=> /(_ c). rewrite /count_occ -/(count_occ _).
-    move=> ->. by case: (Nat.eq_dec a c).
-Admitted.
-*)
-
 
 Lemma in_intersectI {a A B} : In a A -> In a B -> In a (mset_intersect A B).
 Proof.
@@ -482,63 +324,9 @@ Proof.
   by lia.
 Qed.
 
-(*
-Lemma in_intersectI {a A B} : In a A -> In a B -> In a (mset_intersect A B).
-Proof.
-  elim: A B.
-    done.
-  move=> b A IH B. move /in_cons_iff. case.
-    move=> -> /try_remove_in [C]. rewrite /mset_intersect -/mset_intersect.
-    move=> ->. by left.
-  move=> /IH H /H{H}. => ? ?. rewrite /mset_intersect -/mset_intersect.
-  case: (mset_try_remove b B).
-    move=> C. right. apply: IH. 
-    move /
-Admitted.
-*)
-
-(*
-Lemma in_intersect {a A B} : In a (mset_intersect A B) <-> (In a A /\ In a B).
-Proof.
-Admitted.
-*)
-
-Lemma intersect_repeat {a n A} : n > 0 -> In a A -> mset_intersect (repeat a n) A = [a].
-Proof.
-Admitted.
 
 
-Lemma seq_succ {m n} : seq m (S n) = m :: (seq (S m) n).
-Proof.
-  done.
-Qed.
 
-
-Lemma flat_map_cons {X Y : Type} {f : X -> list Y} {a A} : 
-  flat_map f (a :: A) = (f a) ++ (flat_map f A).
-Proof.
-  done.
-Qed.
-
-Lemma repeat_succ {X : Type} {x : X} {n} : repeat x (S n) = x :: repeat x n.
-Proof.
-  done.
-Qed.
-
-Lemma repeat_add {X : Type} {x : X} {m n} : repeat x (m + n) = repeat x m ++ repeat x n.
-Proof.
-  elim: m.
-    done.
-  move=> m IH. cbn. by f_equal.
-Qed.
-
-Lemma repeat_eq_length {X : Type} {x : X} {m n} : (m = n) <-> (repeat x m = repeat x n).
-Proof.
-Admitted.
-
-Lemma repeat_map {X Y: Type} {f: X -> Y} {x: X} {n} : repeat (f x) n = map f (repeat x n).
-Proof.
-Admitted.
 
 (* forces instance to be a sequence *)
 (* first constraint of encode bound *)
@@ -574,7 +362,7 @@ Proof.
     rewrite ? map_app map_cons /plus. by mset_eq_trivial.
   move /eq_cons_iff /IH.
   apply: unnest.
-    rewrite ? app_length cons_length. by lia.
+    rewrite ? app_length => /=. by lia.
   move => ?.
   under (eq_lr (A' := m :: (A1 ++ A2)) (B' := m :: (seq 0 m))).
   all: rewrite -/(mset_eq _ _).
@@ -645,6 +433,8 @@ Proof.
     move=> ?. by lia.
   move/Exists_exists => [a [? ?]]. exfalso.
   have ?: 0 <> a by lia. have: a < n by lia.
+  move=> ?.
+  have: 0 <= a < 0 + n by lia.
   move /in_seq => H. move: HC => /eq_in_iff /(_ a) /iffRL /(_ H){H} => ?.
   have H: In a (mset_intersect C A) by apply: in_intersectI.
   move: (Heq a) => /iffLR. rewrite in_app_iff.
@@ -699,35 +489,11 @@ Proof.
   move: HA' => /eq_length. rewrite app_length seq_length. by lia. 
 Qed.
 
-(* there exist solutions for nat contraints *)
-Lemma nat_sat {n}:
-  let A := flat_map (fun i => repeat i ((Nat.pred n) - i)) (seq 0 n) in
-  (seq 0 n) ++ A ≡ (repeat 0 n) ++ (map S A).
-Proof.
-  move=> A. subst A.
-  elim: n.
-    by done.
-  move=> n IH /=.
-  apply /eq_cons_iff. rewrite ? Nat.sub_0_r.
-  under (eq_lr _ eq_refl (A' := repeat 0 n ++ seq 1 n ++ flat_map (fun i : nat => repeat i (n - i)) (seq 1 n))).
-  all: rewrite -/(mset_eq _ _).
-    by mset_eq_trivial.
-  apply /eq_app_iff. 
-  move: IH => /(eq_map_iff (f := S)).
-  
-  rewrite <- seq_shift. rewrite ? flat_map_concat_map.
-  rewrite ? map_app concat_map. rewrite ? map_map.
-  move /(eq_lr _ _). apply.
-  - apply /eq_app_iff. apply: eq_eq. f_equal. apply: map_ext => a.
-    rewrite repeat_map. do 2 f_equal. by lia.
-  - apply /eq_app_iff. apply: eq_eq. do 2 f_equal. apply: map_ext => a.
-    rewrite repeat_map. do 2 f_equal. by lia. 
-Qed.
 
 (* [0] ++ [0,1] ++ [0,1,2] ++ ...*)
 Definition pyramid n := flat_map (seq 0) (seq 0 n).
 
-Lemma nat_sat_pyramid {n}: (seq 0 n) ++ (pyramid n) ≡ (repeat 0 n) ++ (map S (pyramid n)).
+Lemma nat_sat {n}: (seq 0 n) ++ (pyramid n) ≡ (repeat 0 n) ++ (map S (pyramid n)).
 Proof.
   elim: n.
     by done.
@@ -762,7 +528,7 @@ Qed.
 
 Opaque embed unembed.
 
-Definition encode_nat (x: nat) : MsetU_PROBLEM :=
+Definition encode_nat (x: nat) : FMsetU_PROBLEM :=
   let X := embed (x, 0) in
   let XX := embed (x, 1) in
   let Ax := embed (x, 2) in
@@ -863,7 +629,7 @@ Proof.
   split.
     by apply: @bound_sat.
   split.
-    by apply: nat_sat_pyramid.
+    by apply: nat_sat.
   split.
     by apply: encode_nat_sat_aux.
   done.
@@ -876,7 +642,7 @@ Definition encode_constraint x y z :=
   let yy := embed (y, 1) in
   let y := embed (y, 0) in
   let z := embed (z, 0) in
-  [ (•[0] ⊍ x ⊍ yy ⊍ yy ⊍ y, mset_var z) ].
+  [ (•[0] ⊍ x ⊍ yy ⊍ yy ⊍ y, mset_term_var z) ].
 
 (* if mset constraint has a solution, then uniform diophantine constraint is satisfied *)
 Lemma encode_constraint_spec {φ x y z} : mset_sat φ (encode_constraint x y z) -> 
@@ -920,7 +686,7 @@ Require Import Problems.H10UC.
 
 Definition encode_h10uc '(x, y, z) := encode_constraint x y z.
 
-Lemma H10UC_to_MsetU : H10UC_SAT ⪯ MsetU_SAT.
+Lemma H10UC_to_MsetU : H10UC_SAT ⪯ FMsetU_SAT.
 Proof.
   exists (flat_map encode_h10uc).
   move=> h10ucs. constructor.
---
+++ Problems/FMsetU.v
@@ -0,0 +1,84 @@
+(* 
+  Autor(s):
+    Andrej Dudenhefner (1) 
+  Affiliation(s):
+    (1) Saarland Informatics Campus, Saarland University, Saarbrücken, Germany
+*)
+
+(* 
+  Problem:
+    Finite multiset constraint unification (FMsetU)
+
+  Finite multiset constratins with one constant 0 and one unary constructor h
+  A finite multiset A is represented by a list of elements.
+  The element (h^n 0) is represented by the natural number n.
+
+  Terms t, u:
+    t, u ::= n : list nat | x : nat | t ⊍ u | t ⩀ u | h (t)
+    where x ranges over multiset variables
+  
+  Constraints:
+    t ≐ u 
+
+  FMsetU:
+    Given a list (t₁ ≐ u₁),...,(tₙ ≐ uₙ) of constraints,
+    is there a valuation φ : nat -> list nat such that
+    φ(t₁) ≡ φ(u₁),..., φ(tₙ) ≡ φ(uₙ),
+    where ≡ is equality up to permutation?
+  
+  References:
+    [1] Paliath Narendran: Solving Linear Equations over Polynomial Semirings.
+      LICS 1996: 466-472, doi: 10.1109/LICS.1996.561463
+*)
+
+Require Import PeanoNat.
+Require Import List.
+Import ListNotations.
+
+(* terms *)
+Inductive mset_term : Set :=
+  | mset_term_const : list nat -> mset_term
+  | mset_term_var : nat -> mset_term
+  | mset_term_plus : mset_term -> mset_term -> mset_term
+  | mset_term_cap : mset_term -> mset_term -> mset_term
+  | mset_term_h : mset_term -> mset_term.
+
+(* list equality up to permutation *)
+Definition mset_eq (A B : list nat) : Prop := 
+  forall c, count_occ Nat.eq_dec A c = count_occ Nat.eq_dec B c.
+Notation "A ≡ B" := (mset_eq A B) (at level 65).
+
+(* removes the first occurrence of n in A, fails otherwise *)
+Fixpoint mset_try_remove (n : nat) (A : list nat) : option (list nat) :=
+  match A with
+  | [] => None
+  | (m :: A) => if Nat.eqb n m then Some A else option_map (cons m) (mset_try_remove n A)
+  end.
+
+(* multiset intersection *)
+Fixpoint mset_intersect (A B : list nat) : list nat :=
+  match A with
+  | [] => []
+  | (n :: A) => 
+    match mset_try_remove n B with
+    | None => mset_intersect A B
+    | Some(B) => n :: (mset_intersect A B)
+    end
+  end.
+
+(* evaluate an mset wrt. a valuation φ *)
+Fixpoint mset_sem (φ : nat -> list nat) (A : mset_term) : list nat :=
+  match A with
+    | mset_term_const l => l
+    | mset_term_var x => φ x
+    | mset_term_plus A B => (mset_sem φ A) ++ (mset_sem φ B)
+    | mset_term_cap A B => mset_intersect (mset_sem φ A) (mset_sem φ B)
+    | mset_term_h A => map S (mset_sem φ A)
+  end.
+
+(* list of constraints *)
+Definition FMsetU_PROBLEM := list (mset_term * mset_term).
+
+(* is there a valuation φ that satisfies all contraints *)
+Definition FMsetU_SAT (l : FMsetU_PROBLEM) := 
+  exists (φ : nat -> list nat), forall (A B : mset_term), In (A, B) l -> (mset_sem φ A) ≡ (mset_sem φ B).
