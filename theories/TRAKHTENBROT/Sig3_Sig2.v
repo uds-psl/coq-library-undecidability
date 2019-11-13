3897fcd84288f03034e5cb34b64e1e0c940bb2b8
Dominique Larchey-Wendling <larchey@gmail.com>
2019-11-13 16:49:28 +0100

    half of the reduction from SAT3 to SAT2 this part does not require finiteness

--- theories/TRAKHTENBROT/Sig3_Sig2.v
+++ theories/TRAKHTENBROT/Sig3_Sig2.v
@@ -19,7 +19,7 @@ From Undecidability.Shared.Libs.DLW.Wf
   Require Import wf_finite.
 
 From Undecidability.TRAKHTENBROT
-  Require Import notations fol_ops fo_terms fo_logic.
+  Require Import notations fol_ops fo_terms fo_logic hfs.
 
 Set Implicit Arguments.
 
@@ -51,21 +51,45 @@ Section Sig3_Sig2.
   Definition Σ2_incl x y := ∀ 0 ∈ (S x) ⤑ 0 ∈ (S y).
   Definition Σ2_equiv x y := ∀ 0 ∈ (S x) ↔ 0 ∈ (S y).
 
+  Definition m2_member a b := fom_rels M2 tt (a##b##ø).
+
+  Notation "x '∈m' y" := (m2_member x y) (at level 59, no associativity).
+
   Infix "≈" := Σ2_equiv.
   Infix "⊆" := Σ2_incl.
 
+  Notation "⟪ A ⟫" := (fun ψ => fol_sem M2 ψ A).
+
+  Fact Σ2_incl_spec x y ψ : ⟪Σ2_incl x y⟫ ψ = m2_incl m2_member (ψ x) (ψ y).
+  Proof. reflexivity. Qed.
+
+  Fact Σ2_equiv_spec x y ψ : ⟪Σ2_equiv x y⟫ ψ = m2_equiv m2_member (ψ x) (ψ y).
+  Proof. reflexivity. Qed. 
+ 
   (* is_otriple t x y z := exists p, is_opair p x y /\ is_opair t p z. *)
 
   Definition Σ2_is_pair p x y : fol_form Σ2 := ∀ 0 ∈ (S p) ↔ 0 ≈ S x ⟇ 0 ≈ S y.
+
+  Fact Σ2_is_pair_spec p x y ψ : ⟪Σ2_is_pair p x y⟫ ψ = m2_is_pair m2_member (ψ p) (ψ x) (ψ y).
+  Proof. reflexivity. Qed.
 
   Definition Σ2_is_opair p x y := ∃∃ Σ2_is_pair 1    (2+x) (2+x)
                                    ⟑ Σ2_is_pair 0    (2+x) (2+y)
                                    ⟑ Σ2_is_pair (2+p) 1     0.
 
+  Fact Σ2_is_opair_spec p x y ψ : ⟪Σ2_is_opair p x y⟫ ψ = m2_is_opair m2_member (ψ p) (ψ x) (ψ y).
+  Proof. reflexivity. Qed.
+
   Definition Σ2_is_otriple p x y z := ∃ Σ2_is_opair 0     (S x) (S y)
                                       ⟑ Σ2_is_opair (S p)  0    (S z).
 
-  Definition Σ2_is_triple_in r x y z := ∃ Σ2_is_otriple 0 (S x) (S y) (S z) ⟑ 0 ∈ (S r).
+  Fact Σ2_is_otriple_spec p x y z ψ : ⟪Σ2_is_otriple p x y z⟫ ψ = m2_is_otriple m2_member (ψ p) (ψ x) (ψ y) (ψ z).
+  Proof. reflexivity. Qed.
+
+  Definition Σ2_is_otriple_in r x y z := ∃ Σ2_is_otriple 0 (S x) (S y) (S z) ⟑ 0 ∈ (S r).
+
+  Fact Σ2_is_otriple_in_spec r x y z ψ : ⟪Σ2_is_otriple_in r x y z⟫ ψ = m2_is_otriple_in m2_member (ψ r) (ψ x) (ψ y) (ψ z).
+  Proof. reflexivity. Qed.
 
   Definition Σ3_var : fo_term nat (ar_syms Σ3) -> nat.
   Proof.
@@ -75,9 +99,9 @@ Section Sig3_Sig2.
   Fixpoint Σ3_Σ2 (l r : nat) (A : fol_form Σ3) : fol_form Σ2 :=
     match A with
       | ⊥              => ⊥
-      | fol_atom _ _ v => Σ2_is_triple_in r (Σ3_var (vec_head v)) 
-                                            (Σ3_var (vec_head (vec_tail v)))
-                                            (Σ3_var (vec_head (vec_tail (vec_tail v))))
+      | fol_atom _ _ v => Σ2_is_otriple_in r (Σ3_var (vec_head v)) 
+                                             (Σ3_var (vec_head (vec_tail v)))
+                                             (Σ3_var (vec_head (vec_tail (vec_tail v))))
       | fol_bin b A B  => fol_bin b (Σ3_Σ2 l r A) (Σ3_Σ2 l r B)
       | fol_quant fol_fa A  => ∀ 0 ∈ (S l) ⤑ Σ3_Σ2 (S l) (S r) A
       | fol_quant fol_ex A  => ∃ 0 ∈ (S l) ⟑ Σ3_Σ2 (S l) (S r) A
@@ -93,34 +117,36 @@ Section Sig3_Sig2.
 
   Definition HR1 (l r : M2) := forall x, exists y, fom_rels M2 tt (y##l##ø) /\ R x y.
   Definition HR2 (l r : M2) := forall y, fom_rels M2 tt (y##l##ø) -> exists x, R x y.
-  Definition HR4 (l r : M2) := forall a b c a' b' c' ψ,
+  Definition HR4 (l r : M2) := forall a b c a' b' c',
                                                R a a'
                                             -> R b b'
                                             -> R c c' 
                                             -> fom_rels M3 tt (a##b##c##ø)
-                                           <-> fol_sem M2 ψ↑r↑a'↑b'↑c' (Σ2_is_triple_in 3 2 1 0).
+                                           <-> m2_is_otriple_in m2_member r a' b' c'.
 
   (** Notice the following in HR4, the value of ψ is arbitrary *)
 
-  Fact Σ2_is_triple_in_vars r x y z : incl (fol_vars (Σ2_is_triple_in r x y z)) (r::x::y::z::nil).
+  Fact Σ2_is_otriple_in_vars r x y z : incl (fol_vars (Σ2_is_otriple_in r x y z)) (r::x::y::z::nil).
   Proof. intros a; simpl; tauto. Qed.
 
-  Fact Σ2_is_triple_in_equiv r x y z φ ψ :
-               fol_sem M2 φ↑r↑x↑y↑z (Σ2_is_triple_in 3 2 1 0)
-           <-> fol_sem M2 ψ↑r↑x↑y↑z (Σ2_is_triple_in 3 2 1 0).
+  Fact Σ2_is_otriple_in_equiv r x y z φ ψ :
+               ⟪Σ2_is_otriple_in 3 2 1 0⟫ φ↑r↑x↑y↑z
+           <-> ⟪Σ2_is_otriple_in 3 2 1 0⟫ ψ↑r↑x↑y↑z.
   Proof.
     apply fol_sem_ext.
     intros n Hn.
-    apply Σ2_is_triple_in_vars in Hn.
+    apply Σ2_is_otriple_in_vars in Hn.
     revert Hn.
     repeat (intros [ <- | H ]; [ simpl; auto | revert H ]).
     simpl; tauto.
   Qed.
 
+  Notation "⟪ A ⟫'" := (fun φ => fol_sem M3 φ A) (at level 1, format "⟪ A ⟫'").
+
   Theorem Σ3_Σ2_correct (A : fol_form Σ3) l r φ ψ :
             HR1 (ψ l) (ψ r) -> HR2 (ψ l) (ψ r) -> HR4 (ψ l) (ψ r)
         -> (forall x, In x (fol_vars A) -> R (φ x) (ψ x))
-        -> fol_sem M3 φ A <-> fol_sem M2 ψ (Σ3_Σ2 l r A).
+        -> ⟪ A ⟫' φ <-> ⟪Σ3_Σ2 l r A⟫ ψ.
   Proof.
     revert l r φ ψ.
     induction A as [ | [] | b A HA B HB | [] A HA ]; intros l r phi psy H1 H2 H4 H.
@@ -159,10 +185,114 @@ Section Sig3_Sig2.
        + intros G1; simpl in G1; revert G1; rew fot; intros G1.
          unfold Σ3_Σ2; simpl Σ3_var.
          red in H4.
-         rewrite (@H4 _ _ _ (psy a) (psy b) (psy c) psy) in G1; auto.
+         rewrite (@H4 _ _ _ (psy a) (psy b) (psy c)) in G1; auto.
        + unfold Σ3_Σ2; simpl Σ3_var; intros G1.
          simpl; rew fot.
-         rewrite (@H4 _ _ _ (psy a) (psy b) (psy c) psy); auto. }
+         rewrite (@H4 _ _ _ (psy a) (psy b) (psy c)); auto. }
   Qed.
 
+  Definition Σ2_list_in l lv := let f x A := x ∈ l ⟑ A in fold_right f (⊥⤑⊥) lv.
+
+  Fact Σ2_list_in_spec l lv ψ : ⟪Σ2_list_in l lv⟫ ψ 
+                            <-> forall x, In x lv -> ψ x ∈m ψ l.
+  Proof.
+    induction lv as [ | x lv IH ]; simpl.
+    + split; tauto.
+    + split.
+      * intros (H1 & H2) ? [ <- | H ]; auto.
+        apply IH; auto.
+      * intros H; split.
+        - apply H; auto.
+        - apply IH; intros; apply H; auto.
+  Qed.
+
+  Definition Σ2_extensional := ∀∀∀ 2 ≈ 1 ⤑ 2 ∈ 0 ⤑ 1 ∈ 0.
+
+  Fact Σ2_extensional_spec ψ : ⟪Σ2_extensional⟫ ψ = m2_member_ext m2_member.
+  Proof. reflexivity. Qed.
+
+  Definition Σ2_has_otriples l :=
+    ∀∀∀ 2 ∈ (3+l) ⤑ 1 ∈ (3+l) ⤑ 0 ∈ (3+l) ⤑ ∃ Σ2_is_otriple 0 3 2 1.
+
+  Fact Σ2_has_otriples_spec l ψ : ⟪Σ2_has_otriples l⟫ ψ = m2_has_otriples m2_member (ψ l).
+  Proof. reflexivity. Qed.
+
+  Definition Σ2_non_empty l := ∃ 0 ∈ (1+l).
+
+  Fact Σ2_non_empty_spec l ψ : ⟪Σ2_non_empty l⟫ ψ = exists x, m2_member x (ψ l).
+  Proof. reflexivity. Qed.
+
+  Variable A : fol_form Σ3.
+
+  Let B := fol_subst (fun v => £ (2+v)) A.
+  Let l := 0.
+  Let r := 1.
+
+  Definition Σ3_Σ2_enc := Σ2_extensional ⟑ Σ2_non_empty l
+                        ⟑ Σ2_list_in l (fol_vars B) ⟑ Σ3_Σ2 l r B.
+
 End Sig3_Sig2.
+
+Definition SAT Σ (A : fol_form Σ) := exists (M : fo_model Σ) φ, fol_sem M φ A.
+
+Section SAT2_SAT3.
+
+  Section nested.
+
+    Variables (A : fol_form (Σrel 3))
+              (M2 : fo_model (Σrel 2))
+              (ψ : nat -> M2)
+              (HA : fol_sem M2 ψ (Σ3_Σ2_enc A)).
+
+    Let mem := m2_member M2.
+
+    Let M3 : fo_model (Σrel 3).
+    Proof.
+      exists (sig (fun x => mem x (ψ 0))).
+      + intros [].
+      + intros [] v.
+        simpl in v.
+        apply (m2_is_otriple_in mem (ψ 1)).
+        * exact (proj1_sig (vec_head v)).
+        * exact (proj1_sig (vec_head (vec_tail v))).
+        * exact (proj1_sig (vec_head (vec_tail (vec_tail v)))).
+    Defined.
+
+    Let R (x : M3) (y : M2) := proj1_sig x = y.
+
+    Local Lemma SAT2_to_SAT3 : SAT A.
+    Proof.
+      destruct HA as (H1 & H2 & H3 & H4).
+      rewrite Σ2_non_empty_spec in H2.
+      rewrite Σ2_list_in_spec in H3.
+      destruct H2 as (x0 & H0).
+      set (phi := fun n : nat => 
+        match in_dec eq_nat_dec n (fol_vars A⦃fun v : nat => in_var (2 + v)⦄) with 
+          | left H  => (exist _ (ψ n) (H3 _ H) : M3)
+          | right _ => (exist _ x0 H0 : M3)
+        end).
+      exists M3, (fun n => phi (2+n)).
+      rewrite <- Σ3_Σ2_correct with (φ := phi) (R := R) in H4.
+      + rewrite fol_sem_subst in H4.
+        revert H4; apply fol_sem_ext; intro; rew fot; auto.
+      + intros (x & Hx); exists x; unfold R; simpl; split; auto.
+      + intros x Hx; exists (exist _ x Hx); red; simpl; auto.
+      + intros (a & Ha) (b & Hb) (c & Hc) a' b' c'; unfold R; simpl.
+        intros <- <- <-; tauto.
+      + intros n Hn; red.
+        unfold phi.
+        destruct (in_dec eq_nat_dec n (fol_vars A⦃fun v : nat => in_var (2 + v)⦄))
+          as [ H | [] ]; auto; simpl.
+    Qed.
+
+  End nested.
+
+  Theorem SAT3_SAT2 A : SAT (Σ3_Σ2_enc A) -> SAT A.
+  Proof.
+    intros (M2 & psy & HA).
+    apply SAT2_to_SAT3 with M2 psy; auto.
+  Qed.
+
+End SAT2_SAT3.
+
+Check SAT3_SAT2.
--- theories/TRAKHTENBROT/hfs.v
+++ theories/TRAKHTENBROT/hfs.v
@@ -39,11 +39,11 @@ Section axioms.
   Infix "∈" := R.
   Notation "x ∉ y" := (~ x ∈ y).
 
-  Definition incl x y := forall a, a ∈ x -> a ∈ y.
-  Definition equiv x y := forall a, a ∈ x <-> a ∈ y.
+  Definition m2_incl x y := forall a, a ∈ x -> a ∈ y.
+  Definition m2_equiv x y := forall a, a ∈ x <-> a ∈ y.
 
-  Infix "⊆" := incl.
-  Infix "≈" := equiv. 
+  Infix "⊆" := m2_incl.
+  Infix "≈" := m2_equiv. 
   Notation "x ≉ y" := (~ x ≈ y).
 
   (** Hypothesis on the model *)
@@ -51,13 +51,13 @@ Section axioms.
   Variable (Rdec : forall x y, { x ∈ y } + { x ∉ y })
            (lX : list X) (HX : forall x, In x lX).
 
-  Fact incl_refl x : x ⊆ x.
+  Fact m2_incl_refl x : x ⊆ x.
   Proof. red; auto. Qed.
 
-  Fact incl_trans x y z : x ⊆ y -> y ⊆ z -> x ⊆ z.
-  Proof. unfold incl; auto. Qed.
+  Fact m2_incl_trans x y z : x ⊆ y -> y ⊆ z -> x ⊆ z.
+  Proof. unfold m2_incl; auto. Qed.
 
-  Fact incl_choose x y : { z | z ∈ x /\ z ∉ y } + { x ⊆ y }.
+  Fact m2_incl_choose x y : { z | z ∈ x /\ z ∉ y } + { x ⊆ y }.
   Proof.
     set (P z := z ∈ x /\ z ∉ y).
     set (Q z := z ∈ x -> z ∈ y).
@@ -68,122 +68,123 @@ Section axioms.
     + right; intros z; apply H; auto.
   Qed.  
 
-  Fact incl_dec x y : { x ⊆ y } + { ~ x ⊆ y }.
+  Fact m2_incl_dec x y : { x ⊆ y } + { ~ x ⊆ y }.
   Proof.
-    destruct (incl_choose x y) as [ (?&?&?) |]; auto.
+    destruct (m2_incl_choose x y) as [ (?&?&?) |]; auto.
   Qed.
 
-  Fact equiv_eq x y : x ≈ y <-> x ⊆ y /\ y ⊆ x.
+  Fact m2_equiv_eq x y : x ≈ y <-> x ⊆ y /\ y ⊆ x.
   Proof. firstorder. Qed.
   
-  Fact equiv_dec x y : { x ≈ y } + { x ≉ y }.
+  Fact m2_equiv_dec x y : { x ≈ y } + { x ≉ y }.
   Proof.
-    destruct (incl_dec x y); [ destruct (incl_dec y x) | ].
-    1: left; apply equiv_eq; auto. 
-    all: right; rewrite equiv_eq; tauto.
+    destruct (m2_incl_dec x y); [ destruct (m2_incl_dec y x) | ].
+    1: left; apply m2_equiv_eq; auto. 
+    all: right; rewrite m2_equiv_eq; tauto.
   Qed.
 
-  Fact equiv_refl_True x : x ≈ x <-> True.
-  Proof. unfold equiv; tauto. Qed.
+  Fact m2_equiv_refl_True x : x ≈ x <-> True.
+  Proof. unfold m2_equiv; tauto. Qed.
 
-  Fact equiv_refl x : x ≈ x.
-  Proof. unfold equiv; tauto. Qed.
+  Fact m2_equiv_refl x : x ≈ x.
+  Proof. unfold m2_equiv; tauto. Qed.
 
-  Fact equiv_sym x y : x ≈ y -> y ≈ x.
-  Proof. do 2 rewrite equiv_eq; tauto. Qed.
+  Fact m2_equiv_sym x y : x ≈ y -> y ≈ x.
+  Proof. do 2 rewrite m2_equiv_eq; tauto. Qed.
 
-  Fact equiv_trans x y z : x ≈ y -> y ≈ z -> x ≈ z.
-  Proof. repeat rewrite equiv_eq; unfold incl; intros [] []; split; auto. Qed.
+  Fact m2_equiv_trans x y z : x ≈ y -> y ≈ z -> x ≈ z.
+  Proof. repeat rewrite m2_equiv_eq; unfold m2_incl; intros [] []; split; auto. Qed.
 
-  Add Parametric Relation: (X) (equiv)
-      reflexivity proved by equiv_refl
-      symmetry proved by equiv_sym
-      transitivity proved by equiv_trans
-    as equiv_rst.
+  Add Parametric Relation: (X) (m2_equiv)
+      reflexivity proved by m2_equiv_refl
+      symmetry proved by m2_equiv_sym
+      transitivity proved by m2_equiv_trans
+    as m2_equiv_rst.
 
-  Hint Resolve equiv_refl equiv_sym.
+  Hint Resolve m2_equiv_refl m2_equiv_sym.
 
   (* A first FOL axiom: sets are characterized by their elements *)
 
-  Variable (ax_equiv : forall x y z, x ≈ y -> x ∈ z -> y ∈ z).
+  Definition m2_member_ext := forall x y z, x ≈ y -> x ∈ z -> y ∈ z.
 
-  Fact equiv_mem x y : x ≈ y -> forall z, x ∈ z <-> y ∈ z.
-  Proof. split; apply ax_equiv; auto. Qed.
+  Variable (m2_ax_ext : m2_member_ext).
+
+  Fact m2_equiv_mem x y : x ≈ y -> forall z, x ∈ z <-> y ∈ z.
+  Proof. split; apply m2_ax_ext; auto. Qed.
   
    Add Parametric Morphism: (R) with signature 
-     (equiv) ==> (equiv) ==> (iff) as mem_congr.
+     (m2_equiv) ==> (m2_equiv) ==> (iff) as mem_congr.
   Proof.
     intros x y H1 a b H2; red in H1, H2; split;
-     rewrite <- H2; apply ax_equiv; auto.
+     rewrite <- H2; apply m2_ax_ext; auto.
   Qed.
 
   Add Parametric Morphism: (fun x y => x ⊆ y) with signature 
-     (equiv) ==> (equiv) ==> (iff) as is_incl_congr.
+     (m2_equiv) ==> (m2_equiv) ==> (iff) as is_incl_congr.
   Proof.
     intros x y H1 a b H2; split; intros H z.
     + rewrite <- H1, <- H2; auto.
     + rewrite H1, H2; auto.
   Qed.
 
-  Definition is_pair p x y := forall a, a ∈ p <-> a ≈ x \/ a ≈ y.
+  Definition m2_is_pair p x y := forall a, a ∈ p <-> a ≈ x \/ a ≈ y.
 
-  Definition is_triple t x y z := forall a, a ∈ t <-> a ≈ x \/ a ≈ y \/ a ≈ z.
+  Definition m2_is_triple t x y z := forall a, a ∈ t <-> a ≈ x \/ a ≈ y \/ a ≈ z.
 
-  Fact is_pair_comm p x y : is_pair p x y -> is_pair p y x.
-  Proof. unfold is_pair; apply forall_equiv; intro; tauto. Qed.
+  Fact m2_is_pair_comm p x y : m2_is_pair p x y -> m2_is_pair p y x.
+  Proof. unfold m2_is_pair; apply forall_equiv; intro; tauto. Qed.
 
-  Add Parametric Morphism: (is_pair) with signature 
-     (equiv) ==> (equiv) ==> (equiv) ==> (iff) as is_pair_congr.
+  Add Parametric Morphism: (m2_is_pair) with signature 
+     (m2_equiv) ==> (m2_equiv) ==> (m2_equiv) ==> (iff) as is_pair_congr.
   Proof.
     intros p q H1 x x' H2 y y' H3.
-    unfold is_pair.
     apply forall_equiv; intros a.
     rewrite H1, H2, H3; tauto.
   Qed.
 
-  Add Parametric Morphism: (is_triple) with signature 
-     (equiv) ==> (equiv) ==> (equiv) ==> (equiv) ==> (iff) as is_triple_congr.
+  Add Parametric Morphism: (m2_is_triple) with signature 
+     (m2_equiv) ==> (m2_equiv) ==> (m2_equiv) ==> (m2_equiv) ==> (iff) as is_triple_congr.
   Proof.
     intros p q H1 x x' H2 y y' H3 z z' H4.
-    unfold is_triple.
     apply forall_equiv; intros a.
     rewrite H1, H2, H3, H4; tauto.
   Qed.
 
-  Fact is_pair_fun p q x y : is_pair p x y -> is_pair q x y -> p ≈ q.
+  Fact m2_is_pair_fun p q x y : m2_is_pair p x y -> m2_is_pair q x y -> p ≈ q.
   Proof.
     intros H1 H2; red in H1, H2; intro; 
     rewrite H1, H2; tauto.
   Qed.
 
-  Fact is_triple_fun r t x y z : is_triple r x y z 
-                              -> is_triple t x y z 
-                              -> r ≈ t.
+  Fact m2_is_triple_fun r t x y z : m2_is_triple r x y z 
+                                 -> m2_is_triple t x y z 
+                                 -> r ≈ t.
   Proof.
     intros H1 H2; red in H1, H2; intro;
     rewrite H1, H2; tauto.
   Qed.
 
-  Fact is_pair_inj p x y x' y' : is_pair p x y 
-                              -> is_pair p x' y' 
-                              -> x ≈ x' /\ y ≈ y'
-                              \/ x ≈ y' /\ y ≈ x'.
+  Fact m2_is_pair_inj p x y x' y' : m2_is_pair p x y 
+                                 -> m2_is_pair p x' y' 
+                                 -> x ≈ x' /\ y ≈ y'
+                                 \/ x ≈ y' /\ y ≈ x'.
   Proof.
-    unfold is_pair; intros H1 H2.
-    generalize (proj1 (H2 x)) (proj1 (H2 y)); rewrite H1, H1, equiv_refl_True, equiv_refl_True.
-    generalize (proj1 (H1 x')) (proj1 (H1 y')); rewrite H2, H2, equiv_refl_True, equiv_refl_True.
+    unfold m2_is_pair; intros H1 H2.
+    generalize (proj1 (H2 x)) (proj1 (H2 y)); rewrite H1, H1, m2_equiv_refl_True, m2_equiv_refl_True.
+    generalize (proj1 (H1 x')) (proj1 (H1 y')); rewrite H2, H2, m2_equiv_refl_True, m2_equiv_refl_True.
     intros [] [] [] []; auto.
   Qed.
 
-  Fact is_pair_inj' p x y : is_pair p x x 
-                         -> is_pair p y y
-                         -> x ≈ y.
+  Fact m2_is_pair_inj' p x y : m2_is_pair p x x 
+                            -> m2_is_pair p y y
+                            -> x ≈ y.
   Proof.
-    intros H1 H2; generalize (is_pair_inj H1 H2); tauto.
+    intros H1 H2; generalize (m2_is_pair_inj H1 H2); tauto.
   Qed.
 
   (** Replace the big disj with permutation up-to equivalence *)
 
+  (**
 
   Ltac collect_equiv :=
    repeat match goal with 
@@ -227,7 +228,7 @@ Section axioms.
       | |- True -> _ => intros _ 
       | |- ?a \/ ?b \/ ?c -> _ => let H := fresh in intros [ H | [ H | H ]]; rewrite H
     end.
-(*
+
   The idea of encoding a triple as {{x},{x,y},{x,y,z}} seems to generates
   too many cases to show bijections
 
@@ -269,82 +270,87 @@ Section axioms.
 *)
 
 
-  Definition is_opair p x y := exists a b, is_pair a x x /\ is_pair b x y /\ is_pair p a b.
+  Definition m2_is_opair p x y := exists a b, m2_is_pair a x x /\ m2_is_pair b x y /\ m2_is_pair p a b.
 
-  Add Parametric Morphism: (is_opair) with signature 
-     (equiv) ==> (equiv) ==> (equiv) ==> (iff) as is_opair_congr.
+  Add Parametric Morphism: (m2_is_opair) with signature 
+     (m2_equiv) ==> (m2_equiv) ==> (m2_equiv) ==> (iff) as m2_is_opair_congr.
   Proof.
     intros p q H1 x x' H2 y y' H3.
-    unfold is_opair.
     apply exists_equiv; intros a.
     apply exists_equiv; intros b.
     rewrite H1, H2, H3; tauto.
   Qed.
 
-  Fact is_opair_fun p q x y : is_opair p x y -> is_opair q x y -> p ≈ q.
+  Fact m2_is_opair_fun p q x y : m2_is_opair p x y -> m2_is_opair q x y -> p ≈ q.
   Proof.
     intros (a & b & H1 & H2 & H3) (u & v & G1 & G2 & G3).
-    generalize (is_pair_fun H1 G1) (is_pair_fun H2 G2); intros E1 E2.
+    generalize (m2_is_pair_fun H1 G1) (m2_is_pair_fun H2 G2); intros E1 E2.
     rewrite E1, E2 in H3.
-    revert H3 G3; apply is_pair_fun.
+    revert H3 G3; apply m2_is_pair_fun.
   Qed.
 
-  Fact is_opair_inj p x y x' y' : is_opair p x y 
-                               -> is_opair p x' y' 
-                               -> x ≈ x' /\ y ≈ y'.
+  Fact m2_is_opair_inj p x y x' y' : m2_is_opair p x y 
+                                  -> m2_is_opair p x' y' 
+                                  -> x ≈ x' /\ y ≈ y'.
   Proof.
     intros (a & b & H1 & H2 & H3) (u & v & G1 & G2 & G3).
-    generalize (is_pair_inj H3 G3); intros [ (E1 & E2) | (E1 & E2) ].
+    generalize (m2_is_pair_inj H3 G3); intros [ (E1 & E2) | (E1 & E2) ].
     + rewrite E1 in H1; rewrite E2 in H2.
-      generalize (is_pair_inj' H1 G1); intros E; split; auto.
+      generalize (m2_is_pair_inj' H1 G1); intros E; split; auto.
       rewrite E in H2.
-      generalize (is_pair_inj H2 G2); intros [ | (E3 & E4) ]; try tauto.
+      generalize (m2_is_pair_inj H2 G2); intros [ | (E3 & E4) ]; try tauto.
       rewrite E4; auto.
     + rewrite E1 in H1; rewrite E2 in H2.
-      generalize (is_pair_inj H2 G1) (is_pair_inj H1 G2).
+      generalize (m2_is_pair_inj H2 G1) (m2_is_pair_inj H1 G2).
       intros [ (E3 & E4) | (E3 & E4) ] [ (E5 & E6) | (E5 & E6) ];
         rewrite E4, <- E5; auto.
   Qed.
 
-  Definition is_otriple t x y z := exists p, is_opair p x y /\ is_opair t p z.
+  Definition m2_is_otriple t x y z := exists p, m2_is_opair p x y /\ m2_is_opair t p z.
  
-  Add Parametric Morphism: (is_otriple) with signature 
-     (equiv) ==> (equiv) ==> (equiv) ==> (equiv) ==> (iff) as is_otriple_congr.
+  Add Parametric Morphism: (m2_is_otriple) with signature 
+     (m2_equiv) ==> (m2_equiv) ==> (m2_equiv) ==> (m2_equiv) ==> (iff) as m2_is_otriple_congr.
   Proof.
     intros p q H1 x x' H2 y y' H3 z z' H4.
-    unfold is_otriple.
+    unfold m2_is_otriple.
     apply exists_equiv; intros t.
     rewrite H1, H2, H3, H4; tauto.
   Qed.
 
-  Fact is_otriple_fun p q x y z : is_otriple p x y z -> is_otriple q x y z -> p ≈ q.
+  Fact m2_is_otriple_fun p q x y z : m2_is_otriple p x y z -> m2_is_otriple q x y z -> p ≈ q.
   Proof.
     intros (t1 & H1 & H2) (t2 & H3 & H4).
-    generalize (is_opair_fun H1 H3); intros E.
+    generalize (m2_is_opair_fun H1 H3); intros E.
     rewrite E in H2.
-    apply (is_opair_fun H2 H4).
+    apply (m2_is_opair_fun H2 H4).
   Qed.
 
-  Fact is_otriple_inj t x y z x' y' z' : 
-             is_otriple t x y z 
-          -> is_otriple t x' y' z' 
+  Fact m2_is_otriple_inj t x y z x' y' z' : 
+             m2_is_otriple t x y z 
+          -> m2_is_otriple t x' y' z' 
           -> x ≈ x' /\ y ≈ y' /\ z ≈ z'.
   Proof.
     intros (p & H1 & H2) (q & H3 & H4).
-    destruct (is_opair_inj H2 H4) as (H5 & H6).
+    destruct (m2_is_opair_inj H2 H4) as (H5 & H6).
     rewrite H5 in H1.
-    generalize (is_opair_inj H1 H3); tauto.
+    generalize (m2_is_opair_inj H1 H3); tauto.
   Qed.
 
-  Definition has_pairs (l : X) :=
-     forall x y, x ∈ l -> y ∈ l -> exists p, is_pair p x y.
+  Definition m2_has_pairs (l : X) :=
+     forall x y, x ∈ l -> y ∈ l -> exists p, m2_is_pair p x y.
 
-  Definition powset (l m : X) := forall x, x ∈ m <-> x ⊆ l.
+  Definition m2_has_otriples (l : X) :=
+    forall x y z, x ∈ l -> y ∈ l -> z ∈ l -> exists t, m2_is_otriple t x y z.
 
-  Add Parametric Morphism: (powset) with signature 
-     (equiv) ==> (equiv) ==> (iff) as is_powset_congr.
+  Definition m2_is_otriple_in r x y z :=
+    exists t, m2_is_otriple t x y z /\ t ∈ r.
+
+  Definition m2_powset (l m : X) := forall x, x ∈ m <-> x ⊆ l.
+
+  Add Parametric Morphism: (m2_powset) with signature 
+     (m2_equiv) ==> (m2_equiv) ==> (iff) as m2_is_powset_congr.
   Proof.
-    intros x y H1 a b H2; unfold powset.
+    intros x y H1 a b H2; unfold m2_powset.
     apply forall_equiv; intro.
     rewrite H1, H2; tauto.
   Qed.
@@ -354,30 +360,30 @@ Section axioms.
   Variable (empty : exists x, forall y, y ∉ x).
  
   Variable (l0 l1 l2 l3 : X).     (** a layered model of level 4 *)
-  Variable (pset1 : powset l0 l1)
-           (pset2 : powset l1 l2)
-           (pset3 : powset l2 l3)
-           (hp0 : has_pairs l0)   (** pairs at every level below 3 *)
-           (hp1 : has_pairs l1)
-           (hp2 : has_pairs l2).
+  Variable (pset1 : m2_powset l0 l1)
+           (pset2 : m2_powset l1 l2)
+           (pset3 : m2_powset l2 l3)
+           (hp0 : m2_has_pairs l0)   (** pairs at every level below 3 *)
+           (hp1 : m2_has_pairs l1)
+           (hp2 : m2_has_pairs l2).
 
-  Fact is_pair_layer p x y l pl : x ∈ l -> y ∈ l -> is_pair p x y -> powset l pl -> p ∈ pl.
+  Fact m2_is_pair_layer p x y l pl : x ∈ l -> y ∈ l -> m2_is_pair p x y -> m2_powset l pl -> p ∈ pl.
   Proof.
     intros H1 H2 H3 H4; apply H4; red in H3.
     intros k; rewrite H3; intros [ H | H ]; rewrite H; auto.
   Qed.
 
-  Fact is_opair_l2 p x y : x ∈ l0 -> y ∈ l0 -> is_opair p x y -> p ∈ l2.
+  Fact m2_is_opair_l2 p x y : x ∈ l0 -> y ∈ l0 -> m2_is_opair p x y -> p ∈ l2.
   Proof.
     intros H1 H2 (a & b & H3 & H4 & H5).
-    apply is_pair_layer with (4 := pset1) in H3; auto.
-    apply is_pair_layer with (4 := pset1) in H4; auto.
-    apply is_pair_layer with (4 := pset2) in H5; auto.
+    apply m2_is_pair_layer with (4 := pset1) in H3; auto.
+    apply m2_is_pair_layer with (4 := pset1) in H4; auto.
+    apply m2_is_pair_layer with (4 := pset2) in H5; auto.
   Qed.
    
   Variable (r : X) (Hr : r ∈ l3). (** r encodes a binary relation *)
 
-  Definition P x y := x ∈ l0 /\ y ∈ l0 /\ exists c, c ∈ r /\ is_opair c x y. 
+  Definition m2_P x y := x ∈ l0 /\ y ∈ l0 /\ exists c, c ∈ r /\ m2_is_opair c x y. 
 
   (** We want to show that (encode A) satisfied in this model
            -> A true in the model where P as defined above 
