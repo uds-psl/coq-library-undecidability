19ba62357158ab917756f09ea4dfcabd1aba109a
Dominique Larchey-Wendling <larchey@gmail.com>
2020-03-02 10:51:49 +0100

    small updates

--- theories/MuRec/ra_mm.v
+++ theories/MuRec/ra_mm.v
@@ -215,7 +215,7 @@ Section ra_mm_comp.
 
   Theorem ra_mm_compiler (n : nat) (f : recalg n) :
       { m & { P : list (mm_instr (pos (n+S m))) 
-            |  (forall x v, ⟦f⟧ v x -> exists j, (1,P) /v/ (1,vec_app v vec_zero) ~~> (j,vec_app v (x##vec_zero)))
+            |  (forall x v, ⟦f⟧ v x -> (1,P) /v/ (1,vec_app v vec_zero) ->> (1+length P,vec_app v (x##vec_zero)))
             /\ (forall v, (1,P) /v/ (1,vec_app v vec_zero) ↓ -> ex (⟦f⟧ v)) } }.
   Proof.
     destruct ra_mm_env_simulator with (f := f) as (P & HP).
@@ -239,13 +239,12 @@ Section ra_mm_comp.
         - intros j Hj; unfold get_env.
           destruct (le_lt_dec n j); omega.
         - destruct (H1 _ H) as (e' & G1 & k & G2); auto.
-          exists (1+length P); split.
-          2: rewrite HQ; unfold mm_pos_nat; rew length; simpl; omega.
           exists k.
           rewrite HQ in G2 at 1.
           apply sss_steps_mm_pos_nat_inv with (v1 := vec_app v vec_zero) in G2.
           ++ destruct G2 as (v2 & G2 & G3); simpl in G3.
              eq goal G3; do 2 f_equal.
+             { rewrite HQ; unfold mm_pos_nat; rewrite map_length; auto. }
              apply vec_pos_ext, pos_left_right_rect.
              ** intros p; rewrite G2, vec_pos_app_left, G1; simpl.
                 rewrite pos2nat_left.
