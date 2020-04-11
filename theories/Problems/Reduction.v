aed0a05fe19d885d13305ab491f7634df213e843
Dominique Larchey-Wendling <larchey@gmail.com>
2020-04-11 22:44:34 +0200

    informative and non-informative reductions

--- theories/Problems/Reduction.v
+++ theories/Problems/Reduction.v
@@ -4,43 +4,68 @@ Require Import Setoid.
 
 Set Implicit Arguments.
 
-(** DLW: replace with INFORMATIVE reductions
-    We prove informative reductions in here *)
+(** ** Definitions *)
 
-(* Definition reduces X Y (p : X -> Prop) (q : Y -> Prop) := exists f : X -> Y, forall x, p x <-> q (f x).
-Notation "p ⪯ q" := (reduces p q) (at level 50). *)
-
-(** ** Definition *)
-
-Definition reduces X Y (P : X -> Prop) (Q : Y -> Prop) :=
-        { f : X -> Y | forall x, P x <-> Q (f x) }.
+Definition reduces X Y (p : X -> Prop) (q : Y -> Prop) := exists f : X -> Y, forall x, p x <-> q (f x).
+Definition ireduces X Y (P : X -> Prop) (Q : Y -> Prop) := { f : X -> Y | forall x, P x <-> Q (f x) }.
 
 Infix "⪯" := reduces (at level 70).
+Infix "⪯i" := ireduces (at level 70).
 
 (** ** Pre-order properties *)
 
-Lemma reduces_reflexive X (P : X -> Prop) : P ⪯ P.
-Proof. exists (fun x => x); tauto. Qed.
+Section Properties.
 
-Fact reduces_transitive X P Y Q Z R :
-        @reduces X Y P Q -> @reduces Y Z Q R -> P ⪯ R.
-Proof. 
-  intros (f & Hf) (g & Hg).
-  exists (fun x => g (f x)).
-  intro; rewrite Hf, Hg; tauto.
-Qed.
+  Variables (X : Type) (P : X -> Prop)
+            (Y : Type) (Q : Y -> Prop)
+            (Z : Type) (R : Z -> Prop).
 
-(** ** An equivalent dependent definition *)
+  Fact reduces_reflexive : P ⪯ P.
+  Proof. exists (fun x => x); tauto. Qed.
 
-Fact reduction_dependent X Y (P : X -> Prop) (Q : Y -> Prop) :
-         (P ⪯ Q -> forall x, { y | P x <-> Q y })
-       * ((forall x, { y | P x <-> Q y }) -> P ⪯ Q).
-Proof.
-  split.
-  + intros (f & Hf).
-    intros x; exists (f x); auto.
-  + intros f.
-    exists (fun x => proj1_sig (f x)).
-    intros; apply (proj2_sig (f x)).
-Qed.
+  Fact ireduces_reflexive : P ⪯i P.
+  Proof. exists (fun x => x); tauto. Qed.
+
+  Fact reduces_transitive : P ⪯ Q -> Q ⪯ R -> P ⪯ R.
+  Proof. 
+    intros (f & Hf) (g & Hg).
+    exists (fun x => g (f x)).
+    intro; rewrite Hf, Hg; tauto.
+  Qed.
+
+  Fact ireduces_transitive : P ⪯i Q -> Q ⪯i R -> P ⪯i R.
+  Proof. 
+    intros (f & Hf) (g & Hg).
+    exists (fun x => g (f x)).
+    intro; rewrite Hf, Hg; tauto.
+  Qed.
+
+  Fact ireduces_reduces : P ⪯i Q -> P ⪯ Q.
+  Proof. intros (f & ?); exists f; auto. Qed.
+
+  Fact reduces_ireduces : P ⪯ Q -> inhabited (P ⪯i Q).
+  Proof. intros (f & ?); exists; exists f; auto. Qed.
+
+  Fact reduces_ireduces_iff : P ⪯ Q <-> inhabited (P ⪯i Q).
+  Proof.
+    split.
+    + apply reduces_ireduces.
+    + intros []; apply ireduces_reduces; auto.
+  Qed.
+
+  (** ** An equivalent dependent definition *)
+
+  Fact ireduces_dependent :
+         (P ⪯i Q -> forall x, { y | P x <-> Q y })
+       * ((forall x, { y | P x <-> Q y }) -> P ⪯i Q).
+  Proof.
+    split.
+    + intros (f & Hf).
+      intros x; exists (f x); auto.
+    + intros f.
+      exists (fun x => proj1_sig (f x)).
+      intros; apply (proj2_sig (f x)).
+  Qed.
+
+End Properties.
 
