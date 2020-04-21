a11462bf58d284ab72db1bb79ff58a6a71c13fda
Yannick Forster <forster@ps.uni-saarland.de>
2020-04-21 17:36:56 +0200

    Add definition of synthetic undecidability

--- theories/PCP/PCP_undec.v
+++ theories/PCP/PCP_undec.v
@@ -1,4 +1,5 @@
 Require Import Undecidability.PCP.PCP.
+Require Import Undecidability.Synthetic.Undecidability.
 
 From Undecidability.PCP.Reductions Require 
   SR_to_MPCP MPCP_to_MPCPb MPCP_to_PCP PCP_to_PCPb PCPb_iff_iPCPb PCPb_iff_dPCPb.
@@ -9,8 +10,10 @@ Require Import Undecidability.Problems.Reduction.
 Require Undecidability.SR.SR_undec.
 
 (** The modified Post correspondence problem is undecidable. *)
-Lemma MPCP_undec : HaltTM 1 ⪯ MPCP.
+Lemma MPCP_undec : undecidable MPCP.
 Proof.
+  eapply undecidability_from_reducibility.
+  eapply undecidability_HaltTM.
   eapply reduces_transitive.
   exact SR_undec.SR_undec.
   exact SR_to_MPCP.reduction.
@@ -19,19 +22,19 @@ Qed.
 Check MPCP_undec.
 
 (** The modified Post correspondence problem restricted to binary strings is undecidable. *)
-Lemma MPCPb_undec : HaltTM 1 ⪯ MPCPb.
+Lemma MPCPb_undec : undecidable MPCPb.
 Proof.
-  eapply reduces_transitive.
-  exact MPCP_undec.
+  eapply undecidability_from_reducibility.
+  eapply MPCP_undec.
   exact MPCP_to_MPCPb.reduction.
 Qed.
 
 Check MPCPb_undec.
 
 (** The Post correspondence problem is undecidable. *)
-Lemma PCP_undec : HaltTM 1 ⪯ PCP.
+Lemma PCP_undec : undecidable PCP.
 Proof.
-  eapply reduces_transitive.
+  eapply undecidability_from_reducibility.
   exact MPCP_undec.
   exact MPCP_to_PCP.reduction.
 Qed.
@@ -39,9 +42,9 @@ Qed.
 Check PCP_undec.
 
 (** The Post correspondence problem restricted to binary strings is undecidable. *)
-Lemma PCPb_undec : HaltTM 1 ⪯ PCPb.
+Lemma PCPb_undec : undecidable PCPb.
 Proof.
-  eapply reduces_transitive.
+  eapply undecidability_from_reducibility.
   exact PCP_undec.
   exact PCP_to_PCPb.reduction.
 Qed.
@@ -49,9 +52,9 @@ Qed.
 Check PCPb_undec.
 
 (** The Post correspondence problem restricted to binary strings is undecidable. *)
-Lemma iPCPb_undec : HaltTM 1 ⪯ iPCPb.
+Lemma iPCPb_undec : undecidable iPCPb.
 Proof.
-  eapply reduces_transitive.
+  eapply undecidability_from_reducibility.
   exact PCPb_undec.
   exists id. exact PCPb_iff_iPCPb.PCPb_iff_iPCPb.
 Qed.
@@ -59,9 +62,9 @@ Qed.
 Check iPCPb_undec.
 
 (** The Post correspondence problem restricted to binary strings is undecidable. *)
-Lemma dPCPb_undec : HaltTM 1 ⪯ dPCPb.
+Lemma dPCPb_undec : undecidable dPCPb.
 Proof.
-  eapply reduces_transitive.
+  eapply undecidability_from_reducibility.
   exact PCPb_undec.
   exists id. exact PCPb_iff_dPCPb.PCPb_iff_dPCPb.
 Qed.
---
+++ theories/Synthetic/Undecidability.v
@@ -0,0 +1,18 @@
+Require Import Undecidability.Synthetic.Definitions.
+Require Import Undecidability.TM.Halting.
+
+Definition undecidable {X} (p : X -> Prop) :=
+  decidable p -> decidable (HaltTM 1).
+
+Lemma undecidability_HaltTM :
+  undecidable (HaltTM 1).
+Proof.
+  intros H. exact H.
+Qed.
+
+Lemma undecidability_from_reducibility {X} {p : X -> Prop} {Y} {q : Y -> Prop} :
+  undecidable p -> p ⪯ q -> undecidable q.
+Proof.
+  unfold undecidable, decidable, decider, reduces, reduction, reflects.
+  intros H [f Hf] [d Hd]. eapply H. exists (fun x => d (f x)). intros x. rewrite Hf. eapply Hd.
+Qed.
