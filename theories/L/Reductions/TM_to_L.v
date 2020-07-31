daa7f9fe62cf276e7869700c083bc8757f787d96
Yannick Forster <forster@ps.uni-saarland.de>
2020-07-31 19:16:50 +0200

    Add L_undec file

--- theories/L/Reductions/TM_to_L.v
+++ theories/L/Reductions/TM_to_L.v
@@ -2,13 +2,30 @@ From Undecidability Require Import TM.TMEncoding TM.TMinL TM.Util.TM_facts.
 From Undecidability.L Require Import Computability.MuRec.
 Require Import Undecidability.FOL.Reductions.
 
+Lemma TM_eval_halts Σ n (M : TM Σ n) q t q' t' :
+  TM.eval M q t q' t' -> halt M q' = true.
+Proof.
+  induction 1; eauto.
+Qed.
+
 (** ** Reducing halting problem for TMs to halting problem for L *)
-Theorem Halt_eva :
-  Halt ⪯ converges.
+Theorem HaltMTM_to_HaltL :
+  HaltMTM ⪯ HaltL.
 Proof.
   eexists (fun '(existT2 (Sigma, n) M tp) =>
              (L.app mu (@ext _ _ _ (term_test (mk_mconfig (start M) tp))))).
-  intros [ [Sigma n] M tp ]. cbn.  eapply Halt_red.
+  intros [ [Sigma n] M tp ]. cbn.
+  unfold HaltL. setoid_rewrite eval_iff.
+  split.
+  - intros H.
+    epose proof (Halt_red (mk_mconfig (start M) tp)) as [[v [H1 H2]] _].
+    + destruct H as (? & ? & ?). eexists (mk_mconfig _ _).
+      rewrite <- TM_eval_iff. split. 2:eassumption.
+      eapply TM_eval_halts. eassumption.
+    + exists v. split; eauto.
+      eapply equiv_lambda. eassumption. eassumption.
+  - intros [t [H1 H2]].
+    epose proof (Halt_red (mk_mconfig (start M) tp)) as [_ [[q' t']]].
+    + eexists. eauto.
+    + exists q', t'. eapply TM_eval_iff. eapply H.
 Qed.
-
-(*Print Assumptions Halt_eva.*)
