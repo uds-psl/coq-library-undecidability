0b3818f49147e2f892eca5fe7513de2cb20ec50d
Yannick Forster <forster@ps.uni-saarland.de>
2020-12-17 12:01:04 +0100

    fix compilation

--- theories/TM/Code/List/Length.v
+++ theories/TM/Code/List/Length.v
@@ -239,7 +239,7 @@ Section Lenght.
       hnf. cbn. do 2 eexists. repeat split; eauto.
       now intros _ _ _.
     }
-    Unshelve. 2:eassumption. exact _.
+    Unshelve. 3:eassumption. exact _.
   Qed.
 
 End Lenght.
