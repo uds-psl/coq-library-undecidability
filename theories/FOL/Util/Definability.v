a7c1897e298b52046901060309dc51ab074f7138
Dominik Kirst <kirst@ps.uni-saarland.de>
2020-12-23 13:41:13 +0100

    finished completeness

--- theories/FOL/Util/Definability.v
+++ theories/FOL/Util/Definability.v
@@ -573,7 +573,7 @@ Section Deduction.
     rewrite rm_const_fm_subst. erewrite subst_ext. reflexivity. now intros [].
   Qed.
 
-  Lemma up_sshift0 (phi : form') t :
+  Lemma eq_sshift1 (phi : form') t :
     phi[sshift 1][up t..] = phi.
   Proof.
     erewrite subst_comp, subst_id; try reflexivity. now intros [].
@@ -592,16 +592,16 @@ Section Deduction.
       assert (H : minZFeq' ⊢ ax_pair') by (apply Ctx; firstorder).
       apply (AllE x) in H. cbn in H. apply (AllE y) in H. cbn in H.
       eapply Weak in H. use_exists' H z. 2: auto. apply (ExI z). cbn.
-      apply (ExI x). cbn. subsimpl. rewrite up_sshift0. apply CI; auto. 
+      apply (ExI x). cbn. subsimpl. rewrite eq_sshift1. apply CI; auto. 
       apply (ExI y). cbn. subsimpl. erewrite !subst_comp, subst_ext. apply CI; auto. now intros [].
     - rewrite (vec_inv1 v). cbn. specialize (IH _ (in_hd v)). use_exists' IH x.
       assert (H : minZFeq' ⊢ ax_union') by (apply Ctx; firstorder). apply (AllE x) in H. cbn in H.
       eapply Weak in H. use_exists' H y. 2: auto. apply (ExI y). cbn. apply (ExI x). cbn. subsimpl.
-      rewrite up_sshift0. apply CI; auto.    
+      rewrite eq_sshift1. apply CI; auto.    
     - rewrite (vec_inv1 v). cbn. specialize (IH _ (in_hd v)). use_exists' IH x.
       assert (H : minZFeq' ⊢ ax_power') by (apply Ctx; firstorder). apply (AllE x) in H. cbn in H.
       eapply Weak in H. use_exists' H y. 2: auto. apply (ExI y). cbn. apply (ExI x). cbn. subsimpl.
-      rewrite up_sshift0. apply CI; auto.  
+      rewrite eq_sshift1. apply CI; auto.  
     - apply Ctx. firstorder.
   Qed.
 
@@ -683,6 +683,12 @@ Section Deduction.
         erewrite !subst_comp, subst_ext. reflexivity. all: now intros [|[]]; cbn.
   Qed.
 
+  Lemma eq_sshift2 (phi : form') t :
+    phi[sshift 2][up (up t..)] = phi[sshift 1].
+  Proof.
+    erewrite subst_comp, subst_ext; try reflexivity. now intros [|].
+  Qed.
+
   Lemma rm_const_tm_sub { p : peirce } A t (x y : term') :
     minZFeq' <<= A -> A ⊢ (rm_const_tm t)[x..] -> A ⊢ (rm_const_tm t)[y..] -> A ⊢ sub' x y.
   Proof.
@@ -691,8 +697,29 @@ Section Deduction.
       eapply minZF_trans; auto. apply (Weak H1); auto. apply minZF_sym; auto. apply (Weak H2). auto.
     - intros H _. prv_all' z. apply II. apply Exp. apply (AllE z) in H. cbn in H. subsimpl_in H.
       eapply IE; try apply (Weak H); auto.
-    - admit.
-    - rewrite (vec_inv1 v). cbn. rewrite !up_sshift0 in *. intros H1 H2. 
+    - rewrite (vec_inv2 v). cbn. rewrite !eq_sshift1. intros H1 H2. prv_all' a. apply II.
+      eapply Weak in H1. use_exists' H1 b. 2: auto. clear H1. assert1 H. apply CE in H as [H1 H3].
+      eapply Weak in H3. use_exists' H3 c. 2: auto. clear H3. assert1 H. apply CE in H as [H3 H4].
+      eapply Weak in H2. use_exists' H2 d. 2: auto. clear H2. assert1 H. apply CE in H as [H2 H5].
+      eapply Weak in H5. use_exists' H5 e. 2: auto. clear H5. assert1 H. apply CE in H as [H5 H6].
+      
+      (* making the goal a bit more readable, could be automated *)
+      pose (B := ((rm_const_tm (Vector.hd v))[b..]
+           ∧ (∃ (rm_const_tm (Vector.hd (Vector.tl v)))[sshift 2][up (up x..)][up b..]
+                ∧ (∀ $0 ∈' x`[↑]`[↑] <--> $0 ≡' b`[↑]`[↑] ∨ $0 ≡' ↑ 0)) :: a ∈' x :: A)).
+      fold B in H1, H2, H3, H4, H5, H6. fold B. rewrite !eq_sshift2, !eq_sshift1 in *.
+
+      assert (HB : minZFeq' <<= B). { rewrite HA. unfold B. auto. }
+      apply (AllE a) in H6. cbn in H6. subsimpl_in H6. eapply IE. eapply CE2. apply H6.
+      apply (AllE a) in H4. cbn in H4. subsimpl_in H4. eapply DE.
+      + eapply IE. eapply CE1. apply (Weak H4); auto. apply Ctx. unfold B. auto.
+      + apply DI1. eapply minZF_trans; auto. apply minZF_ext; auto.
+        * eapply (IH _ (in_hd v)); auto; eapply Weak. apply H1. auto. apply H2. auto.
+        * eapply (IH _ (in_hd v)); auto; eapply Weak. apply H2. auto. apply H1. auto.
+      + apply DI2. eapply minZF_trans; auto. apply minZF_ext; auto.
+        * eapply (IH _ (in_hd_tl v)); auto; eapply Weak. apply H3. auto. apply H5. auto.
+        * eapply (IH _ (in_hd_tl v)); auto; eapply Weak. apply H5. auto. apply H3. auto.
+    - rewrite (vec_inv1 v). cbn. rewrite !eq_sshift1 in *. intros H1 H2. 
       prv_all' a. apply II. eapply Weak in H1. use_exists' H1 b. 2: auto.
       eapply Weak in H2. use_exists' H2 c. 2: auto. clear H1 H2.
       assert1 H. apply CE in H as [H1 H2]. apply (AllE a) in H2. cbn in H2. subsimpl_in H2.
@@ -700,21 +727,21 @@ Section Deduction.
       assert2 H. apply CE in H as [H2 H3]. apply (AllE a) in H3. cbn in H3. subsimpl_in H3.
       apply CE1 in H3. assert3 H4. apply (IE H3) in H4. use_exists' H4 d. clear H3 H4.
       apply (ExI d). cbn. subsimpl. apply CI. 2: eapply CE2; auto.
-      specialize (IH _ (in_hd v) _ _ _ H2 H1). apply (AllE d) in IH. cbn in IH. subsimpl_in IH.
-      eapply Weak in IH. apply (IE IH). 2: auto. eapply CE1; auto.
-    - rewrite (vec_inv1 v). cbn. rewrite !up_sshift0. intros H1 H2.
+      eapply (IH _ (in_hd v) _ b c) in H1; auto. apply (AllE d) in H1. cbn in H1. subsimpl_in H1.
+      eapply Weak in H1. apply (IE H1). 2: auto. eapply CE1; auto.
+    - rewrite (vec_inv1 v). cbn. rewrite !eq_sshift1. intros H1 H2.
       prv_all' a. apply II. eapply Weak in H1. use_exists' H1 b. 2: auto.
       eapply Weak in H2. use_exists' H2 c. 2: auto. clear H1 H2.
       assert1 H. apply CE in H as [H1 H2]. apply (AllE a) in H2. cbn in H2. subsimpl_in H2.
       eapply IE. eapply CE2, H2. clear H2.
       assert2 H. apply CE in H as [H2 H3]. apply (AllE a) in H3. cbn in H3. subsimpl_in H3.
       apply CE1 in H3. assert3 H4. apply (IE H3) in H4. clear H3.
-      prv_all' d. apply II. specialize (IH _ (in_hd v) _ _ _ H2 H1). apply (AllE d) in IH. cbn in IH. subsimpl_in IH.
-      eapply Weak in IH. apply (IE IH). 2: auto. apply (AllE d) in H4. cbn in H4. subsimpl_in H4.
+      prv_all' d. apply II. apply (IH _ (in_hd v) _ b c) in H1; auto. apply (AllE d) in H1. cbn in H1. subsimpl_in H1.
+      eapply Weak in H1. apply (IE H1). 2: auto. apply (AllE d) in H4. cbn in H4. subsimpl_in H4.
       eapply Weak in H4. apply (IE H4). all: auto.
     - intros [H1 H2] % CE [H3 H4] % CE. apply (AllE y) in H2.
       cbn -[is_inductive] in H2. subsimpl_in H2. apply (IE H2). apply H3.
-  Admitted. adsf
+  Qed.
 
   Lemma rm_const_tm_equiv { p : peirce } A t (x y : term') :
     minZFeq' <<= A -> A ⊢ (rm_const_tm t)[x..] -> A ⊢ y ≡' x <-> A ⊢ (rm_const_tm t)[y..].
