bb18b08ecaf3564115d78fda8d4c0a6bc9b6eace
dominik-kirst <kirst@ps.uni-saarland.de>
2021-08-13 15:15:58 +0200

    conservativity backwards

--- theories/FOL/Util/ZF_conservativity.v
+++ theories/FOL/Util/ZF_conservativity.v
@@ -1,11 +1,17 @@
 (** ** Conservativity Results for ZF *)
 
-From Undecidability.FOL.Util Require Import FullTarski_facts Syntax_facts FullDeduction_facts.
-From Undecidability.FOL Require Import ZF minZF Reductions.PCPb_to_ZF Reductions.PCPb_to_minZF.
+From Undecidability.FOL.Util
+     Require Import FullTarski_facts Syntax_facts FullDeduction_facts.
+
+From Undecidability.FOL
+     Require Import ZF minZF Reductions.PCPb_to_ZF Reductions.PCPb_to_minZF Reductions.PCPb_to_ZFD.
+
 From Undecidability Require Import Shared.ListAutomation.
 Import ListAutomationNotations.
 Local Set Implicit Arguments.
 Local Unset Strict Implicit.
+
+Open Scope syn.
 
 
 (* ** Deductive conservativity *)
@@ -81,7 +87,7 @@ Proof.
   eapply Weak; try apply H1. apply loop_deductive.
 Qed.
 
-Theorem conservativity_deductive_Z' { p : peirce } phi :
+Theorem conservativity_ZF' { p : peirce } phi :
   ZFeq' ⊢ embed phi -> minZFeq' ⊢ phi.
 Proof.
   intros H. apply (@rm_const_prv _ nil) in H. cbn in H. now apply loop_deductive'.
@@ -111,7 +117,7 @@ Proof.
   erewrite subst_ext; try reflexivity. now intros [|n].
 Qed.
 
-Theorem conservativity_deductive_Z { p : peirce } phi :
+Theorem conservativity_Z { p : peirce } phi :
   Zeq ⊢T embed phi -> minZeq ⊢T phi.
 Proof.
   intros [A HA] % Z_decomp. apply rm_const_prv in HA.
@@ -158,7 +164,7 @@ Proof.
     erewrite subst_ext; try reflexivity. now intros [|[|n]].
 Qed.
 
-Theorem conservativity_deductive_ZF { p : peirce } phi :
+Theorem conservativity_ZF { p : peirce } phi :
   ZFeq ⊢T embed phi -> minZFeq ⊢T phi.
 Proof.
   intros [A [B HAB]] % ZF_decomp. apply rm_const_prv, loop_deductive' in HAB; auto.
@@ -174,57 +180,140 @@ Proof.
 Qed.
 
 
+(* ** Backwards directions *)
 
-
-
-
-
-
-
-
-
-
-(* ** Semantic conservativity *)
-
-Section Model.
-
-  Open Scope sem.
-
-  Context {V : Type} {I : interp sig_func_empty _ V}.
-
-  Hypothesis M_ZF : forall rho, rho ⊫ minZF'.
-  Variable x0 : V.
-
-  Instance full_model : interp _ _ V.
-  Proof.
-    split.
-    - intros [] _; exact x0.
-    - now apply (@i_atom sig_func_empty).
-  Defined.
-
-  Lemma full_embed_eval (rho : nat -> V) (t : term') :
-    eval rho (embed_t t) = eval rho t.
-  Proof.
-    destruct t as [|[]]. reflexivity.
-  Qed.
-
-  Lemma full_embed (rho : nat -> V) (phi : form')  :
-    sat full_model rho (embed phi) <-> sat I rho phi.
-  Proof.
-    induction phi in rho |- *; try destruct b0; try destruct q; cbn.
-    1,3-7: firstorder. erewrite Vector.map_map, Vector.map_ext.
-    reflexivity. apply full_embed_eval.
-  Qed.
-
-End Model.
-
-Theorem conservativity_semantic phi :
-  entailment_ZF' (embed phi) -> entailment_minZF' phi.
+Lemma prv_embed { p : peirce } A phi :
+  A ⊢ phi -> (map embed A) ⊢ embed phi.
 Proof.
-  intros H D I rho H1 H2. destruct (H2 rho ax_eset') as [x0 _]; try now right.
-  apply full_embed with x0. apply H; try apply H1. intros sigma psi [<-|[<-|[<-|[<-|Hp]]]].
-  - cbn. apply (H2 sigma ax_ext'). now left.
-  - cbn. admit.
-  - cbn.
-Abort.
-  
+  intros H. pattern p, A, phi. revert p A phi H.
+  apply prv_ind_full; cbn; intros; subst; auto. 1,6-9: eauto.
+  - apply AllI. apply (Weak H0).
+    rewrite !map_map. intros psi' [psi [<- HP]] % in_map_iff.
+    apply in_map_iff. eexists. split; try apply HP.
+    rewrite embed_subst. apply subst_ext. reflexivity.
+  - apply (AllE (embed_t t)) in H0. rewrite embed_subst.
+    erewrite subst_ext; try apply H0. now intros [|n].
+  - apply (ExI (embed_t t)). rewrite embed_subst in H0.
+    erewrite subst_ext; try apply H0. now intros [|n].
+  - eapply ExE; try apply H0. rewrite embed_subst in H2.
+    erewrite subst_ext; try apply (Weak H2); try now intros [|n].
+    rewrite !map_map. intros theta' [<-|[theta [<- HP]] % in_map_iff]; auto.
+    right. apply in_map_iff. eexists. split; try apply HP.
+    rewrite embed_subst. apply subst_ext. reflexivity.
+Qed.
+
+Lemma ZF_eset { p : peirce } :
+  ZFeq' ⊢ ∀ ¬ $0 ∈ ∅.
+Proof.
+  assert (H : ZFeq' ⊢ ax_eset) by (apply Ctx; unfold ZFeq', ZF'; eauto 6).
+  prv_all x. apply (AllE x H).
+Qed.
+
+Lemma ZF_inductive_embed { p : peirce } t :
+  ZFeq' ⊢ inductive t <~> (embed (is_inductive $0))[t..].
+Proof.
+  cbn. apply CI; apply II; apply CI.
+  - apply (ExI ∅). cbn. subsimpl. apply CI; try apply (Weak ZF_eset); auto.
+    eapply CE1. apply Ctx. left. reflexivity.
+  - prv_all x. apply II. apply (ExI (σ x)). cbn. subsimpl. apply CI.
+    + admit.
+    + eapply IE; try now apply Ctx. assert2 H.
+      apply CE2, (AllE x) in H. cbn in H. now subsimpl_in H.
+  - assert1 H. apply CE1 in H. use_exists H x. clear H. eapply ZF_eq_elem; auto.
+    2: apply ZF_refl'; auto. 2: eapply CE2; eauto. apply ZF_ext'; auto.
+    + prv_all y. apply II. apply Exp. assert2 H. apply CE1 in H. apply (AllE y) in H.
+      cbn in H. subsimpl_in H. apply (IE H). auto.
+    + prv_all y. apply II. apply Exp. eapply IE; try now apply Ctx. apply ZF_eset'. auto.
+  - prv_all x. assert1 H. apply CE2 in H. apply (AllE x) in H. cbn in H. subsimpl_in H.
+    rewrite imps in *. use_exists H y. clear H. eapply ZF_eq_elem. admit.
+Admitted.
+
+Lemma embed_ZF' { p : peirce } phi :
+  phi el minZFeq' -> ZFeq' ⊢ embed phi.
+Proof.
+  intros [<-|[<-|[<-|[<-|[<-|[<-|[<-|[<-|[<-|[<-|[]]]]]]]]]]].
+  1-5: cbn; apply Ctx; unfold ZFeq', ZF'; eauto.
+  - cbn. apply (ExI ∅). cbn. apply ZF_eset.
+  - assert (H : ZFeq' ⊢ ax_pair) by (apply Ctx; unfold ZFeq', ZF'; eauto 7).
+    cbn. prv_all x. prv_all y. apply (ExI ({x; y})). cbn. subsimpl.
+    apply (AllE y) in H. cbn in H. apply (AllE x) in H. cbn in H. now subsimpl_in H.
+  - assert (H : ZFeq' ⊢ ax_union) by (apply Ctx; unfold ZFeq', ZF'; eauto 8).
+    cbn. prv_all x. apply (ExI (⋃ x)). cbn. subsimpl. apply (AllE x H).
+  - assert (H : ZFeq' ⊢ ax_power) by (apply Ctx; unfold ZFeq', ZF'; eauto 9).
+    cbn. prv_all x. apply (ExI (PP x)). cbn. subsimpl. apply (AllE x H).
+  - cbn. apply (ExI ω). cbn. apply CI.
+    + assert (H : ZFeq' ⊢ ax_om1) by (apply Ctx; unfold ZFeq', ZF'; eauto 10).
+      unfold ax_om1 in H. apply (IE (CE1 (ZF_inductive_embed ω))) in H. apply H.
+    + assert (H : ZFeq' ⊢ ax_om2) by (apply Ctx; unfold ZFeq', ZF'; eauto 11).
+      prv_all x. apply II. apply (AllE x) in H. cbn in H. eapply IE; try apply (Weak H); auto.
+      specialize (CE2 (ZF_inductive_embed x)). intros H'. eapply IE; try apply (Weak H'); auto.
+Qed.
+
+Theorem conservativity_back_ZF' { p : peirce } phi :
+  minZFeq' ⊢ phi -> ZFeq' ⊢ embed phi.
+Proof.
+  intros HP. apply prv_embed in HP. apply (prv_cut HP).
+  intros psi' [psi[<- H]] % in_map_iff. now apply embed_ZF'.
+Qed.
+
+Lemma minZ_decomp { p : peirce } phi :
+  minZeq ⊢T phi -> exists A, (map ax_sep' A ++ minZFeq') ⊢ phi.
+Proof.
+  intros [A [H1 H2]]. induction A in phi, H1, H2 |- *.
+  - exists nil. cbn. eapply Weak; eauto.
+  - rewrite <- imps in H2. apply IHA in H2 as [B H]; auto.
+    rewrite imps in H. destruct (H1 a); auto.
+    + exists B. eapply Weak; try apply H. auto.
+    + exists (phi0 :: B). cbn. apply H.
+Qed.
+
+Theorem conservativity_back_Z { p : peirce } phi :
+  minZeq ⊢T phi -> Zeq ⊢T embed phi.
+Proof.
+  intros [A HA] % minZ_decomp. apply prv_embed in HA.
+  exists ([ax_sep (embed psi) | psi ∈ A] ++ ZFeq'). split.
+  - intros psi [H|H] % in_app_iff; try now constructor.
+    apply in_map_iff in H as [psi'[<- H]]. constructor 2.
+  - apply (prv_cut HA). intros psi' [psi [<- [H|H] % in_app_iff]] % in_map_iff.
+    + apply in_map_iff in H as [theta [<- H]]. apply Ctx. apply in_app_iff. left.
+      apply in_map_iff. exists theta. split; trivial. cbn. unfold ax_sep.
+      rewrite embed_subst. erewrite subst_ext; try reflexivity. now intros [|n].
+    + eapply Weak; try apply embed_ZF'; auto.
+Qed.
+
+Lemma minZF_decomp { p : peirce } phi :
+  minZFeq ⊢T phi -> exists A B, ((map ax_sep' A ++ map ax_rep' B) ++ minZFeq') ⊢ phi.
+Proof.
+  intros [C [H1 H2]]. induction C in phi, H1, H2 |- *.
+  - exists nil, nil. cbn. eapply Weak; eauto.
+  - rewrite <- imps in H2. apply IHC in H2 as [A[B HAB]]; auto.
+    rewrite imps in HAB. destruct (H1 a); auto.
+    + exists A, B. eapply Weak; try apply HAB. auto.
+    + exists (phi0 :: A), B. cbn. apply HAB.
+    + exists A, (phi0 :: B). cbn. eapply Weak; try apply HAB.
+      intros psi [->|[[H|H] % in_app_iff|H] % in_app_iff]; auto.
+      apply in_app_iff. left. apply in_app_iff. right. now right.
+Qed.
+
+Theorem conservativity_back_ZF { p : peirce } phi :
+  minZFeq ⊢T phi -> ZFeq ⊢T embed phi.
+Proof.
+  intros [A[B HAB]] % minZF_decomp. apply prv_embed in HAB.
+  exists ([ax_sep (embed psi) | psi ∈ A] ++ [ax_rep (embed psi) | psi ∈ B] ++ ZFeq'). split.
+  - intros psi [H|[H|H] % in_app_iff] % in_app_iff; try now constructor.
+    + apply in_map_iff in H as [psi'[<- H]]. constructor 2.
+    + apply in_map_iff in H as [psi'[<- H]]. constructor 3.
+  - apply (prv_cut HAB). intros psi' [psi [<- [[H|H] % in_app_iff|H] % in_app_iff]] % in_map_iff.
+    + apply in_map_iff in H as [theta [<- H]]. apply Ctx. apply in_app_iff. left.
+      apply in_map_iff. exists theta. split; trivial. cbn. unfold ax_sep.
+      rewrite embed_subst. erewrite subst_ext; try reflexivity. now intros [|n].
+    + apply in_map_iff in H as [theta [<- H]]. apply Ctx. apply in_app_iff. right.
+      apply in_app_iff. left. apply in_map_iff. exists theta. split; trivial. cbn. unfold ax_rep, fun_rel.
+      rewrite !embed_subst. setoid_rewrite subst_ext at 1. setoid_rewrite subst_ext at 2.
+      setoid_rewrite subst_ext at 3. setoid_rewrite subst_ext at 4. reflexivity.
+      all: now intros [|[|n]].
+    + eapply Weak; try apply embed_ZF'; auto.
+Qed.
+
+
+
