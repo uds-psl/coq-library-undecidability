fbfed642365a493fcc2eed3d0af4d2e1541b3731
Marc <hermesmarc@gmail.com>
2021-08-14 16:26:15 +0200

    update

--- theories/FOL/Util/Friedman.v
+++ theories/FOL/Util/Friedman.v
@@ -34,30 +34,73 @@ Section Expanded.
 
 
   Definition Q {ff} := (@atom funcs_signature preds_signature _ ff Q_ ([])).
-  Definition DN_Q {ff} phi : form := (phi ~> @Q ff) ~> Q.
+  Definition dnQ {ff} phi : form := (phi ~> @Q ff) ~> Q.
 
   Fixpoint Friedman {ff} (phi : @form funcs_signature preds_signature _ ff) : form ff :=
     match phi with
     | falsity => Q
-    | atom P v => match P with Q_ => Q | _ => DN_Q (atom P v) end
+    | atom P v => match P with Q_ => Q | _ => dnQ (atom P v) end
     | bin Impl phi psi => (Friedman phi) ~> (Friedman psi)
     | bin Conj phi psi => (Friedman phi) ∧ (Friedman psi)
-    | bin Disj phi psi => DN_Q ((Friedman phi) ∨ (Friedman psi))
+    | bin Disj phi psi => dnQ ((Friedman phi) ∨ (Friedman psi))
     | quant All phi => ∀ (Friedman phi)
-    | quant Ex phi => DN_Q (∃ (Friedman phi))
+    | quant Ex phi => dnQ (∃ (Friedman phi))
     end.
 
                       
   Notation "'Fr' f" := (Friedman f) (at level 30).
   
-  Variable ff : falsity_flag.
-  Fixpoint List_Fr Gamma := map Friedman Gamma.
+  Definition List_Fr {ff} Gamma := map (@Friedman ff) Gamma.
   
-  Lemma DNE_Q Gamma phi : Gamma ⊢I Fr (DN_Q phi) ~> Fr phi. 
+  Lemma subst_Fr {ff} (phi : @form funcs_signature preds_signature _ ff) sigma:
+    subst_form sigma (Fr phi) = Fr (subst_form sigma phi).
+  Proof.
+    revert sigma.
+    induction phi; cbn.
+    - reflexivity.
+    - destruct P; reflexivity.
+    - destruct b0; cbn; unfold dnQ, Q; congruence.
+    - destruct q; cbn; intros sigma; unfold dnQ, Q; congruence.
+  Qed.
+  
+  
+  Lemma DNE_Q {ff} Gamma phi : Gamma ⊢I Fr (dnQ phi) ~> @Friedman ff phi. 
   Proof. Admitted.
 
-  Lemma Friedman_cl_to_intu Gamma phi : Gamma ⊢C phi -> (List_Fr Gamma) ⊢I Fr phi.
-  Proof. Admitted.
+  
+  Lemma Friedman_cl_to_intu {ff} Gamma phi :
+    Gamma ⊢C phi -> (List_Fr Gamma) ⊢I @Friedman ff phi.
+  Proof.
+    intros H. induction H; unfold List_Fr in *.
+    - cbn. now constructor.
+    - econstructor; eassumption.
+    - cbn. constructor. admit.
+    - cbn in *. eapply AllE with (t0:=t) in IHprv.
+      cbn in *. now rewrite subst_Fr in IHprv. 
+    - cbn. apply II.
+      eapply IE.
+      + apply Ctx. firstorder.
+      + apply Weak with (A0 := map Friedman A); [|firstorder].
+        apply ExI with (t0:=t). now rewrite subst_Fr.
+    - admit.
+    - cbn in *.
+      specialize (DNE_Q (map Friedman A) phi) as H'.
+      eapply IE; [eassumption|].
+      cbn; apply II. eapply Weak; eauto.
+    - now apply Ctx, in_map.
+    - cbn. now apply CI.
+    - cbn in *. eapply CE1; eauto.
+    - cbn in *. eapply CE2; eauto.
+    - cbn. apply II. eapply IE.
+      + apply Ctx. firstorder.
+      + apply DI1. eapply Weak; eauto.
+    - cbn. apply II. eapply IE.
+      + apply Ctx. firstorder.
+      + apply DI2. eapply Weak; eauto.
+    - cbn in *. admit.
+    - specialize (DNE_Q (map Friedman A) (((phi ~> psi) ~> phi) ~> phi)) as H'.
+      eapply IE; [eassumption|]. cbn. apply gen_peirce.
+  Admitted.
 
     
 End Expanded.
