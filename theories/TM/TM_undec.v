f691e85ce4bf82af6fba872da52c73c8aa0682db
Andrej Dudenhefner <mrhaandi@gmail.com>
2021-12-14 14:42:49 +0100

    wip

--- theories/TM/TM_undec.v
+++ theories/TM/TM_undec.v
@@ -2,18 +2,21 @@ Require Import Undecidability.Synthetic.Undecidability.
 Require Import Undecidability.TM.TM.
 
 Require Import Undecidability.TM.Reductions.mTM_to_TM.
-
+Require Import Undecidability.TM.Reductions.L_to_mTM.
+Require Import Undecidability.L.L_undec.
 (** ** HaltMTM and HaltTM 1 are undecidable *)
-
-Lemma HaltTM_1_undec :
-  undecidable (HaltTM 1).
-Proof.
-  intros d. exact d.
-Qed.
 
 Lemma HaltMTM_undec :
   undecidable HaltMTM.
 Proof.
-  apply (undecidability_from_reducibility HaltTM_1_undec).
-  eapply nTM_to_MTM.
+  apply (undecidability_from_reducibility HaltL_undec).
+  apply (reduces_transitive HaltL_HaltTM).
+  apply nTM_to_MTM.
+Qed.
+
+Lemma HaltTM_1_undec :
+  undecidable (HaltTM 1).
+Proof.
+  apply (undecidability_from_reducibility HaltMTM_undec).
+  apply MTM_to_stM.
 Qed.
