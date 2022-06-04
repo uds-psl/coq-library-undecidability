a39d1438291cf5fa98f31bd0ceb629ab40af57eb
Andrej Dudenhefner <mrhaandi@gmail.com>
2022-06-04 21:51:06 +0200

    fix for lia (weaker in 8.15.1)

--- theories/DiophantineConstraints/Reductions/H10SQC_SAT_to_H10UC_SAT.v
+++ theories/DiophantineConstraints/Reductions/H10SQC_SAT_to_H10UC_SAT.v
@@ -130,7 +130,7 @@ Lemma v_spec : φ' (v 0) = 0 /\ φ' (v 1) = 1.
 Proof using Hφ'.
   move: (Hφ'). rewrite -Forall_forall /ucs Forall_app /v012.
   move=> [/Forall_cons_iff [+]] /Forall_cons_iff [+] /Forall_cons_iff [+] _ _ => /=.
-  by lia.
+  by nia.
 Qed.
 
 Lemma h10sqc_of_h10ucs_spec {c} : Forall (h10uc_sem φ') (h10sqc_to_h10ucs c) -> h10sqc_sem φ c.
