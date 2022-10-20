1279b86cd7e53a8e1fe73f4eb7c4495864eda5d1
Dominique Larchey-Wendling <larchey@gmail.com>
2022-10-20 16:08:06 +0200

    WIP: half tapes

--- theories/TM/PCTM_half.v
+++ theories/TM/PCTM_half.v
@@ -16,10 +16,179 @@ Import ListNotations.
 
 Set Implicit Arguments.
 
+Notation "x ↑ n" := (list_repeat x n) (at level 1, no associativity, format "x ↑ n").
+
+Inductive direction : Set := mv_left | mv_right.
+
+Definition half_tape := (list bool * bool * list bool)%type.
+
+Implicit Type t : half_tape.
+
+Reserved Notation "l '~r' m" (at level 70, no associativity).
+
+Inductive half_tape_equiv_right : list bool -> list bool -> Prop :=
+  | ht_right_eq_lft l : l ~r [] -> false::l ~r []
+  | ht_right_eq_rt  l : [] ~r l -> [] ~r false::l
+  | ht_right_eq_bth b l m : l ~r m -> b::l ~r b::m
+where "l ~r m" := (half_tape_equiv_right l m).
+
+Definition half_tape_equiv t1 t2 :=
+  match t1, t2 with
+  | (l₁,h₁,r₁), (l₂, h₂, r₂) => l₁ = l₂ /\ h₁ = h₂ /\ r₁ ~r r₂
+  end.
+
+Infix "~~" := half_tape_equiv (at level 70, no associativity).
+
+Definition half_tape_opt_equiv (ot1 ot2 : option half_tape) :=
+  match ot1, ot2 with
+  | Some t1, Some t2 => t1 ~~ t2
+  | None, None       => True
+  | _   , _          => False
+  end.
+
+Infix "~o" := half_tape_opt_equiv (at level 70, no associativity).
+
+Definition half_tape_left t :=
+  match t with
+  | ([],h,r)   => None
+  | (x::l,h,r) => Some (l,x,h::r)
+  end.
+
+Definition half_tape_right t :=
+  Some (match t with 
+  | (l,h,[])   => (h::l,false,[])
+  | (l,h,x::r) => (h::l,x,r)
+  end).
+
+Definition half_tape_move d :=
+  match d with
+  | mv_left  => half_tape_left
+  | mv_right => half_tape_right
+  end.
+
+Definition half_tape_read t :=
+  match t with
+  | (_,h,_) => h
+  end.
+
+Definition half_tape_write t h : half_tape := 
+  match t with
+  | (l,_,r) => (l,h,r)
+  end.
+
+(** ~~ is a congruence for all tape operations *)
+
+Fact half_tape_left_equiv t1 t2 : t1 ~~ t2 -> half_tape_left t1 ~o half_tape_left t2.
+Proof.
+  revert t1 t2; intros ((l,h),r1) ((l2,h2),r2) (<- & <- & H).
+  destruct l as [ | x l ]; simpl; auto; msplit 2; auto; now constructor.
+Qed.
+
+Fact half_tape_right_equiv t1 t2 : t1 ~~ t2 -> half_tape_right t1 ~o half_tape_right t2.
+Proof.
+  revert t1 t2; intros ((l,h),r1) ((l2,h2),r2) (<- & <- & H).
+  induction H as [ r1 H IH | r2 H IH | b r1 r2 H IH ]; simpl; auto.
+Qed.
+
+Fact half_tape_move_equiv d t1 t2 : t1 ~~ t2 -> half_tape_move d t1 ~o half_tape_move d t2.
+Proof.
+  destruct d; simpl half_tape_move.
+  + apply half_tape_left_equiv.
+  + apply half_tape_right_equiv.
+Qed.
+
+Fact half_tape_read_equiv t1 t2 : t1 ~~ t2 -> half_tape_read t1 = half_tape_read t2.
+Proof. revert t1 t2; intros ([]&?) ([]&?) (_ & ? & _); auto. Qed.
+
+Fact half_tape_write_equiv {t1 t2} h : t1 ~~ t2 -> half_tape_write t1 h ~~ half_tape_write t2 h.
+Proof. revert t1 t2; intros ([]&?) ([]&?) (? & ? & ?); subst; msplit 2; auto. Qed.
+
+(** * Halting problem for (micro-programmed) Turing machines with a PC counter *)
+
+(* Three instructions: MV d | WR b | BR i j *)
+Inductive pctm_instr : Set :=
+  | pctm_combi  : bool -> direction -> nat 
+               -> bool -> direction -> nat 
+               -> pctm_instr.
+
+Module PCTMNotations.
+  Notation rd := half_tape_read.
+  Notation wr := half_tape_write.
+  Notation mv := half_tape_move.
+End PCTMNotations.
+
+Import PCTMNotations.
+
+(* ** Small-step semantics for PC based TM *)
+
+(* state is a value for the PC value and a tape *) 
+Definition pctm_state := (nat * half_tape)%type.
+
+(**    ρ // (i,t₁) -1> (j,t₂) 
+    means instruction ρ at PC i transforms 
+    the state (i,t₁) into the state (j,t₂) *)
+
+Inductive pctm_sss : pctm_instr -> pctm_state -> pctm_state -> Prop :=
+  | in_pctm_sss₀ w₀ d₀ j₀ w₁ d₁ j₁ i t t' : 
+       rd t = false -> mv d₀ (wr t w₀) = Some t' -> pctm_combi w₀ d₀ j₀ w₁ d₁ j₁ // (i,t) -1> (j₀,t')
+  | in_pctm_sss₁ w₀ d₀ j₀ w₁ d₁ j₁ i t t' : 
+       rd t = true  -> mv d₁ (wr t w₁) = Some t' -> pctm_combi w₀ d₀ j₀ w₁ d₁ j₁ // (i,t) -1> (j₁,t')
+where "ρ // s -1> t" := (pctm_sss ρ s t).
+
+Definition pctm_state_equiv (s₁ s₂ : pctm_state) := 
+  match s₁, s₂ with
+  | (i1,t1), (i2,t2) => i1 = i2 /\ t1 ~~ t2
+  end.
+
+Infix "~s" := pctm_state_equiv (at level 70).
+
+Fact pctm_sss_equiv ρ s₁ s₂ s₁' : 
+   ρ // s₁ -1> s₁' -> s₁ ~s s₂ -> exists s₂', ρ // s₂ -1> s₂' /\ s₁' ~s s₂'.
+Proof.
+  destruct s₂ as (i2,t2).
+  induction 1 as [ w0 d0 j0 w1 d1 j1 i t1 t1' H1 H2 | w0 d0 j0 w1 d1 j1 i t1 t1' H1 H2 ]; intros (H3 & H4); simpl; subst i2.
+  + generalize (half_tape_write_equiv w0 H4); intros H5.
+    apply half_tape_move_equiv with (d := d0) in H5.
+    rewrite H2 in H5.
+    case_eq (mv d0 (wr t2 w0)).
+    * intros t2' H'; rewrite H' in H5; simpl in H5. 
+      exists (j0,t2'); split; auto.
+      constructor 1; auto.
+      now apply half_tape_read_equiv in H4 as <-.
+    * intros H; now rewrite H in H5.
+  + generalize (half_tape_write_equiv w1 H4); intros H5.
+    apply half_tape_move_equiv with (d := d1) in H5.
+    rewrite H2 in H5.
+    case_eq (mv d1 (wr t2 w1)).
+    * intros t2' H'; rewrite H' in H5; simpl in H5. 
+      exists (j1,t2'); split; auto.
+      constructor 2; auto.
+      now apply half_tape_read_equiv in H4 as <-.
+    * intros H; now rewrite H in H5.
+Qed.
+
+#[local] Notation "P // s -[ k ]-> t" := (sss_steps pctm_sss P k s t).
+#[local] Notation "P // s ->> t" := (sss_compute pctm_sss P s t).
+#[local] Notation "P // s -+> t" := (sss_progress pctm_sss P s t).
+
+Section displace_1_right.
+
+  Variables (l : list bool) (x : bool) (a : nat) (y : bool) (r : list bool) (t : half_tape) (i : nat) (P : list pctm_instr).
+
+  Fact displace_1a_left : (i,P) // (i,(l,x,true↑a++false::y::r)) -+> (length P+i,(true↑a++false::x::l,false,r)).
+  Admitted.
+
+  Fact 
+
+                       -> exists t' (i,P) // (i,t) 
+
+  
+
+
+
 Definition option_lift X Y (f : X -> option Y) x :=
   match x with Some x => f x | _ => None end.
 
-Inductive direction : Set := mv_left | mv_right.
 
 Fact nth_error_nil X n : @nth_error X [] n = None.
 Proof. induction n; simpl; auto. Qed.  
@@ -52,6 +221,8 @@ Proof.
   + apply IHl; intros n.
     apply (H (S n)).
 Qed.
+
+
 
 Definition half_tape := (list bool * bool * option (list bool))%type.
 
@@ -180,41 +351,4 @@ Section half_tape.
   Qed.
 
 End half_tape.
-
-(** * Halting problem for (micro-programmed) Turing machines with a PC counter *)
-
-(* Three instructions: MV d | WR b | BR i j *)
-Inductive pctm_instr : Set :=
-  | pctm_mv  : direction -> pctm_instr
-  | pctm_wr  : bool -> pctm_instr
-  | pctm_br  : nat -> nat -> pctm_instr
-  .
-
-Module PCTMNotations.
-
-  Notation MV  := pctm_mv.
-  Notation WR  := pctm_wr.
-  Notation BR  := pctm_br.
-
-  Notation rd := half_tape_read.
-  Notation wr := half_tape_write.
-
-End PCTMNotations.
-
-Import PCTMNotations.
-
-(* ** Small-step semantics for PC based TM *)
-
-(* state is a value for the PC value and a tape *) 
-Definition pctm_state := (nat * half_tape)%type.
-
-(**    ρ // (i,t₁) -1> (j,t₂) 
-    means instruction ρ at PC i transforms 
-    the state (i,t₁) into the state (j,t₂) *)
-
-Inductive pctm_sss : pctm_instr -> pctm_state -> pctm_state -> Prop :=
-  | pctm_sss_mv d i t t' :  half_tape_mv d t = Some t' -> MV d // (i,t) -1> (1+i,t')
-  | pctm_sss_wr b i t :                                   WR b // (i,t) -1> (1+i,wr t b)
-  | pctm_sss_br i j p t :                               BR j p // (i,t) -1> (if rd t then j else p,t)
-where "ρ // s -1> t" := (pctm_sss ρ s t).
 
