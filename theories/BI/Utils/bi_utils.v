ed97b10d798f61b148bddd643faa9216c0d8f43f
DmxLarchey <larchey@gmail.com>
2026-03-26 15:41:43 +0100

    high level theorems for encoding ACM2

--- theories/BI/Utils/bi_utils.v
+++ theories/BI/Utils/bi_utils.v
@@ -7,7 +7,7 @@
 (*        Mozilla Public License Version 2.0, MPL-2.0         *)
 (**************************************************************)
 
-From Stdlib Require Import List Utf8.
+From Stdlib Require Import List Utf8 Permutation.
 
 From Undecidability.BI
   Require Import BI.
@@ -15,6 +15,23 @@ From Undecidability.BI
 Import BI_notations.
 
 Set Implicit Arguments.
+
+(*
+#[local] Infix "-∘" := tps_lolipop (at level 65, right associativity).
+#[local] Infix "∘" := tps_tensor (at level 64, left associativity).
+#[local] Infix "⊓" := tps_with (at level 62, left associativity). *)
+#[local] Notation "X ⊆ Y" := (∀m, X m → Y m) (at level 70).
+#[local] Infix "∊" := In (at level 70).
+#[local] Infix "~p" := (@Permutation _) (at level 70).
+
+#[local] Hint Constructors Permutation : core.
+
+Fact permutation_in_head {X} (x : X) l : x ∊ l → ∃m, l ~p x::m.
+Proof.
+  induction l as [ | y l IHl ].
+  + intros [].
+  + intros [ <- | []%IHl ]; eauto.
+Qed.
 
 Arguments BI_ctx_hole {_}.
 
@@ -41,19 +58,19 @@ Section pseudo_exponential.
     now apply BI_spcf_cntr.
   Qed.
 
-  Lemma BI_pseudo_exp_weak Γ γ φ :
-             Γ ⊦ γ 
+  Lemma BI_pseudo_exp_weak Γ γ φ ψ :
+             Γ ⊦ ψ 
       (*------------------*)
-    →    Γ ⊛ₘ ⟨![γ]φ⟩ ⊦ γ.
+    →    Γ ⊛ₘ ⟨![γ]φ⟩ ⊦ ψ.
   Proof.
     intros H.
     set (Δ := BI_ctx_comp true BI_mult Γ BI_ctx_hole).
-    change (Δ[⟨![γ]φ⟩] ⊦ γ).
+    change (Δ[⟨![γ]φ⟩] ⊦ ψ).
     apply BI_spcf_conj_l.
     set (Δ' := BI_ctx_comp true BI_mult Γ (BI_ctx_comp false BI_addi ⟨1⟩ BI_ctx_hole)).
-    change (Δ'[⟨⊤-∗(φ-∗γ)⇒γ⟩] ⊦ γ).
+    change (Δ'[⟨⊤-∗(φ-∗γ)⇒γ⟩] ⊦ ψ).
     apply BI_spcf_weak.
-    change (Δ[øₐ ⊛ₐ ⟨1⟩] ⊦ γ).
+    change (Δ[øₐ ⊛ₐ ⟨1⟩] ⊦ ψ).
     apply BI_spcf_equiv with Δ[⟨1⟩].
     1: apply BI_bequiv_congr, BI_bequiv_sym, BI_bequiv_neut.
     apply BI_spcf_unit_l.
@@ -62,6 +79,28 @@ Section pseudo_exponential.
     apply BI_bequiv_sym,
           BI_bequiv_trans with (1 := BI_bequiv_comm _ _ _),
           BI_bequiv_neut.
+  Qed.
+
+  Definition BI_list_mult := fold_right (λ φ Γ, ⟨φ⟩ ⊛ₘ Γ) øₘ.
+
+  Hint Constructors BI_bunch_equiv : core.
+
+  Fact BI_list_mult_perm_bequiv l m :
+    l ~p m → BI_list_mult l ≡ BI_list_mult m.
+  Proof. induction 1; simpl; eauto. Qed.
+
+  Theorem BI_list_mult_weak Σ Γ ψ :
+      (∀A, A ∊ Σ → ∃ γ φ, A = ![γ]φ)
+    → Γ ⊦ ψ 
+    → (BI_list_mult Σ) ⊛ₘ Γ ⊦ ψ.
+  Proof.
+    intros H1%Forall_forall H; revert H1.
+    induction 1 as [ | A Σ (γ & φ & ->) _ IH ]; simpl.
+    + revert H; apply BI_spcf_equiv.
+      apply BI_bequiv_sym, BI_bequiv_neut.
+    + apply BI_spcf_equiv with (1 := BI_bequiv_sym (BI_bequiv_assoc _ _ _ _)),
+            BI_spcf_equiv with (1 := BI_bequiv_comm _ _ _),
+            BI_pseudo_exp_weak; trivial.
   Qed.
 
   Fact BI_first_idea Γ γ φ : Γ ⊛ₘ ⟨φ⟩ ⊦ γ → Γ ⊛ₐ ⟨(φ-∗γ)⇒γ⟩ ⊦ γ.
@@ -115,6 +154,25 @@ Section pseudo_exponential.
 
   Check BI_pseudo_exp_weak.
   Check BI_pseudo_exp_derilection.
+
+  Theorem BI_list_mult_derilection Σ γ φ :
+      ![γ]φ ∊ Σ
+    → (BI_list_mult Σ) ⊛ₘ ⟨φ⟩ ⊦ γ 
+    → (BI_list_mult Σ) ⊦ γ.
+  Proof.
+    intros (Σ' & H'%BI_list_mult_perm_bequiv)%permutation_in_head H.
+    apply BI_spcf_equiv with (1 := BI_bequiv_sym H').
+    simpl.
+    apply BI_spcf_equiv with (1 := BI_bequiv_comm _ _ _).
+    apply BI_pseudo_exp_derilection.
+    revert H; apply BI_spcf_equiv.
+    do 2 apply BI_bequiv_sym, BI_bequiv_trans with (1 := BI_bequiv_comm _ _ _).
+    apply BI_bequiv_congr.
+    apply BI_bequiv_trans with (1 := H'), BI_bequiv_comm.
+  Qed.
+
+  Check BI_list_mult_weak.
+  Check BI_list_mult_derilection.
 
 End pseudo_exponential.
 
